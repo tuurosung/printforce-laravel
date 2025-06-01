729e4b3117e35ca9235fb49dd7a7eebeb9ace479
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2025-06-01 01:22:10 +0000

    feat: Create CustomersHelper class for customer-related functionalities

---
+++ app/Helpers/CustomerHelpers/CustomersHelper.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Helpers\CustomerHelpers;
+
+use App\Models\Jobs\PressJob;
+use App\Models\Jobs\DesignJob;
+use App\Models\Customers\Customer;
+use App\Models\Jobs\EmbroideryJob;
+use Illuminate\Support\Facades\DB;
+use App\Models\Jobs\LargeFormatJob;
+use App\Models\Jobs\PhotographyJob;
+use App\Models\Customers\CustomerPayment;
+use Illuminate\Database\Query\JoinClause;
+
+class CustomersHelper
+{
+
+
+
+}
