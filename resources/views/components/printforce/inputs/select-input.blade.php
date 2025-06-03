5d68be331b1f447811ea63cff925308cd5c85d85
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2025-06-03 23:58:20 +0000

    feat: Add select input component for improved form handling and user interaction

---
+++ resources/views/components/printforce/inputs/select-input.blade.php
@@ -0,0 +1,12 @@
+<div class="mb-3">
+    <label for="" class="form-label">{{ $label }}</label>
+    <select class="form-select select2-input"
+        name="{{ $name }}"
+        id="{{ $id }}">
+        <option>--- Select Option ---</option>
+
+        @foreach ($options as $key => $value)
+            <option value="{{ $key }}" {{ $selected === $key ? 'selected' : '' }}>{{ $value }}</option>
+        @endforeach
+    </select>
+</div>
