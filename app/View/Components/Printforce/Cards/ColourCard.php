5b8a4fd4e6f300b6c48012b510d25b56ed38fea8
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2025-06-07 03:19:09 +0000

    feat: Enhance ColourCard component with value formatting and styling improvements

--- app/View/Components/Printforce/Cards/ColourCard.php
+++ app/View/Components/Printforce/Cards/ColourCard.php
@@ -15,13 +15,23 @@ class ColourCard extends Component
         public string $title = 'Card Title',
         public $value = 0,
         public string $bgColour = 'primary',
-        public $link = '#'
+        public $link = '#',
+        public $valueType = 'money'
     )
     {
         // if the provided value is neither a float nor an int, default to 0
         // if (!is_float($value) && !is_int($value)) {
         //     $this->value = 0;
         // }
+
+        // if the valueType is money, format the value as a currency
+        if ($this->valueType === 'money') {
+            $this->value = 'GHS '. number_format($this->value, 2);
+        } elseif ($this->valueType === 'count') {
+            $this->value = number_format($this->value);
+        } elseif ($this->valueType === 'percentage') {
+            $this->value = number_format($this->value, 2) . '%';
+        }
     }
 
     /**
--- resources/views/components/printforce/cards/colour-card.blade.php
+++ resources/views/components/printforce/cards/colour-card.blade.php
@@ -2,7 +2,7 @@
     <div class="card-body px-3 pb-2">
         <div class="bg-{{ $bgColour }} bg-opacity-50 px-3 py-3 rounded-3 mb-3">
             <h4>{{ $title }}</h4>
-            <p>{{ $value }}</p>
+            <p class="cal-sans fs-16px">{{ $value }}</p>
         </div>
 
         <a href="{{ $link }}" class="text-decoration-none d-flex">Explore
