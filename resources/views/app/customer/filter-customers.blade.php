0201534ffedb815cf52f86dfadcbdc5a5cd4bbca
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2025-06-10 20:48:04 +0000

    refactor(FilterCustomers): improve code readability by reformatting HTML structure

--- resources/views/app/customer/filter-customers.blade.php
+++ resources/views/app/customer/filter-customers.blade.php
@@ -1,72 +1,67 @@
 <table class="table table-sm datatable">
-                <thead class="">
-                    <tr>
-                        <th>#</th>
-                        <th>Date Created</th>
-                        <th>Customer Name</th>
-                        <th>Type</th>
-                        <th>Phone Number</th>
-                        <th class="text-end">Jobs</th>
-                        <th class="text-end">Paid</th>
-                        <th class="text-end">Balance</th>
-                        <th class="text-end">Option</th>
-                    </tr>
-                </thead>
-                <tbody>
+    <thead class="">
+        <tr>
+            <th>#</th>
+            <th>Date Created</th>
+            <th>Customer Name</th>
+            <th>Type</th>
+            <th>Phone Number</th>
+            <th class="text-end">Jobs</th>
+            <th class="text-end">Paid</th>
+            <th class="text-end">Balance</th>
+            <th class="text-end">Option</th>
+        </tr>
+    </thead>
+    <tbody>
 
-                    @php
-                    $i = 1;
-                    $total_jobs = 0;
-                    $total_payments = 0;
-                    $total_balance = 0;
-                    @endphp
+        @php
+        $i = 1;
+        $total_jobs = 0;
+        $total_payments = 0;
+        $total_balance = 0;
+        @endphp
 
-                    @foreach ($customers as $customer)
+        @foreach ($customers as $customer)
 
-                    <tr>
-                        <td><?php echo $i++; ?></td>
-                        <td>{{ $customer->created_at->format('M d, Y') }}</td>
-                        <td class="text-capitalize" style="text-decoration: underline;">
-                            <a href="{{ route('customers.customer.info', $customer) }}">
+        <tr>
+            <td><?php echo $i++; ?></td>
+            <td>{{ $customer->created_at->format('M d, Y') }}</td>
+            <td class="text-capitalize" style="text-decoration: underline;">
+                <a href="{{ route('customers.customer.info', $customer) }}">
 
-                                {{ strtolower($customer->name) }}
+                    {{ strtolower($customer->name) }}
 
-                            </a>
-                        </td>
-                        <td>{{ $customer->customer_category_description }}</td>
-                        <td><?php echo $customer['phone']; ?></td>
-                        <td class="text-end">
-                            <?php echo number_format($customer->customer_debit, 2); ?>
-                        </td>
-                        <td class="text-end"><?php echo number_format($customer->customer_credit, 2); ?></td>
-                        <td class="text-end"><?php echo number_format($customer->customer_balance, 2); ?></td>
-                        <td class="text-end">
-                            <a
-                                href="/edit-customer/{{ $customer->customer_id }}"
-                                class="text-primary me-3">
-                                <i class="fas fa-pen me-3 text-primary "></i>
-                                Edit
-                            </a>
+                </a>
+            </td>
+            <td>{{ $customer->customer_category_description }}</td>
+            <td><?php echo $customer['phone']; ?></td>
+            <td class="text-end">
+                <?php echo number_format($customer->customer_debit, 2); ?>
+            </td>
+            <td class="text-end"><?php echo number_format($customer->customer_credit, 2); ?></td>
+            <td class="text-end"><?php echo number_format($customer->customer_balance, 2); ?></td>
+            <td class="text-end">
+                <a href="javascript:void(0)" data-url="{{ route('customers.customer.edit', $customer) }}"
+                    class="text-primary edit  me-3">
+                    <i class="fas fa-pen text-primary "></i>
+                    Edit
+                </a>
 
-                            <?php //if ($user->isAdmin()) :
-                            ?>
+                @can('administrator')
+                <form method="POST" action="{{ route('customers.customer.delete', $customer) }}" class="d-inline-block">
+                    @csrf
+                    @method('delete')
+                    <a href="javascript:void(0)" class="text-danger delete">
+                        <i class="fas fa-trash-alt text-danger" aria-hidden="true"></i>
+                        Delete
+                    </a>
+                </form>
+                @endcan
+            </td>
+        </tr>
 
-                            <a
-                                href="#"
-                                class="text-danger delete"
-                                data-url="customer-controller/delete-customer.php?sn=<?php echo $customer['sn']; ?>">
-                                <i class="fas fa-trash-alt text-danger" aria-hidden="true"></i>
-                                Delete
-                            </a>
-
-                            <?php //endif;
-                            ?>
-
-                        </td>
-                    </tr>
-
-                    @endforeach
+        @endforeach
 
 
-                </tbody>
-            </table>
+    </tbody>
+</table>
