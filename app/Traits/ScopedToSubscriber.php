9ef98767a50ff50421612261e83776d5248f62a1
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2025-06-14 13:47:48 +0000

    fix(ScopedToSubscriber): ensure subscriber_id scope only applies when user is authenticated

--- app/Traits/ScopedToSubscriber.php
+++ app/Traits/ScopedToSubscriber.php
@@ -2,14 +2,17 @@
 
 namespace App\Traits;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Builder;
 
 trait ScopedToSubscriber
 {
     public static function bootScopedToSubscriber()
     {
-        static::addGlobalScope('subscriber_id', function (Builder $builder){
-            $builder->where('subscriber_id', auth()->user()->subscriber_id);
+        static::addGlobalScope('subscriber_id', function (Builder $builder) {
+            if (Auth::check()) {
+                $builder->where('subscriber_id', Auth::user()->subscriber_id);
+            }
         });
     }
 }
