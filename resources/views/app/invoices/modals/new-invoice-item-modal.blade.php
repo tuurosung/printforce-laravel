ade5e0dfd9e160a918445682d8af77779632599c
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2025-11-10 12:33:57 +0000

    feat(InvoiceModals): add modals for creating and managing invoices and invoice items

---
+++ resources/views/app/invoices/modals/new-invoice-item-modal.blade.php
@@ -0,0 +1,161 @@
+<div class="modal fade" id="newInvoiceItemModal" tabindex="-1" role="dialog" aria-labelledby="modalTitleId"
+    aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalTitleId">
+                    Add New Item
+                </h5>
+                <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form method="POST" action="{{ route('invoices.invoice-items.store', $customerInvoice) }}">
+                @csrf
+
+                <div class="modal-body">
+
+                <input type="hidden" name="service_category_id" id="serviceCategoryId" value="">
+
+
+                    <div class="row">
+
+                        <div class="col">
+                            <div class="mb-3">
+                                <label for="serviceId" class="form-label">Service</label>
+                                <select class="form-select form-select-sm" name="service_id" id="serviceId" required data-fetch-url="{{ route('configuration.print-services.fetch-service-detail') }}">
+                                    <option value="" selected>--- Select Service ---</option>
+                                    @foreach ($printServices as $service)
+                                    <option value="{{ $service->service_id }}">{{ $service->service_name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="mb-3">
+                                <label for="cost" class="form-label">Unit Cost</label>
+                                <input type="text" class="form-control form-control-sm" name="unit_cost" id="unitCost"
+                                    readonly required />
+                            </div>
+
+                        </div>
+                    </div>
+
+
+                    <div class="row">
+
+
+
+                        <div class="col">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Width</label>
+                                        <input type="number" step="any" class="form-control form-control-sm"
+                                            name="width" id="width" required />
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Height</label>
+                                        <input type="number" step="any" class="form-control form-control-sm"
+                                            name="height" id="height" required />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="mb-3">
+
+                                        <label for="" class="form-label">Measurement</label>
+                                        <select class="form-select form-select-sm" name="measuring_unit"
+                                            id="measuringUnit" required>
+
+                                            <option value="ft">Feet</option>
+                                            <option value="inch">Inch</option>
+
+                                        </select>
+
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Quantity</label>
+                                        <input type="number" class="form-control form-control-sm" name="quantity"
+                                            id="quantity" placeholder="Quantity" min="1" value="1"
+                                            required />
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="mb-3">
+                                        <label for="sub_total" class="form-label">Sub-Total</label>
+                                        <input type="number" class="form-control" name="sub_total" id="subTotal"
+                                            aria-describedby="helpId" placeholder="" readonly/>
+                                    </div>
+
+                                </div>
+                                <div class="col">
+                                    <div class="mb-3">
+                                        <label for="material_unit_cost" class="form-label">Material Unit Cost</label>
+                                        <input type="number" class="form-control" name="material_unit_cost"
+                                            id="materialUnitCost" aria-describedby="" placeholder="" />
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="mb-3">
+                                        <label for="material_total_cost" class="form-label">Material Total Cost</label>
+                                        <input
+                                            type="number"
+                                            class="form-control"
+                                            name="material_total_cost"
+                                            id="materialTotalCost"
+                                            aria-describedby=""
+                                            placeholder=""
+                                            readonly
+                                        />
+                                    </div>
+
+                                </div>
+                                <div class="col">
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Total Cost</label>
+                                        <input type="text" class="form-control form-control-sm" name="total"
+                                            id="total" readonly required />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">
+                        Close
+                    </button>
+                    <button type="submit" class="btn btn-primary">
+                        <i class="fas fa-check me-3  "></i>
+                        Add Item
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
---
+++ resources/views/app/invoices/prepare-invoice.blade.php
@@ -0,0 +1,173 @@
+@extends('layout.app')
+
+@section('content')
+
+
+
+<div class="card border-0 col-lg-6 col-md-8 col-12 col-sm-12 mx-auto" style="min-height:70vh;">
+    <div class="card-body px-5">
+
+        <h2 class="cal-sans fw-500 mb-5">Prepare Invoice</h2>
+
+
+        <div class="d-flex mb-5">
+            <div>
+                <h5>{{ $customerInvoice->customer->name }}</h5>
+                <p class="mb-0">Customer Type : {{ $customerInvoice->customer->category }}</p>
+                <p class="mb-0">{{ $customerInvoice->customer->phone }}</p>
+            </div>
+            <div>
+
+            </div>
+        </div>
+
+        <div class="row mb-4 d-none">
+            <div class="col-2">
+                <div class="mb-3">
+                    <label for="" class="form-label">Apply NHIL</label>
+                    <select class="form-select form-select-lg" name="" id="">
+                        <option selected>Select one</option>
+                        <option value="">New Delhi</option>
+                        <option value="">Istanbul</option>
+                        <option value="">Jakarta</option>
+                    </select>
+                </div>
+
+            </div>
+            <div class="col-2">
+                <div class="mb-3">
+                    <label for="" class="form-label">Apply GetFund</label>
+                    <select class="form-select form-select-lg" name="" id="">
+                        <option selected>Select one</option>
+                        <option value="">New Delhi</option>
+                        <option value="">Istanbul</option>
+                        <option value="">Jakarta</option>
+                    </select>
+                </div>
+
+            </div>
+            <div class="col-2">
+                <div class="mb-3">
+                    <label for="" class="form-label">Apply VAT</label>
+                    <select class="form-select" name="" id="">
+                        <option selected>Select one</option>
+                        <option value="">New Delhi</option>
+                        <option value="">Istanbul</option>
+                        <option value="">Jakarta</option>
+                    </select>
+                </div>
+
+            </div>
+        </div>
+
+        <div>
+            <div class="d-flex justify-content-between align-items-center mb-3">
+                <div>
+                    <h5 class="fw-500 mb-0">Invoice Items</h5>
+                    <p class="mb-0">Add items to the invoice</p>
+                </div>
+                <div>
+                    <button class="btn btn-primary" data-bs-toggle="modal" data-toggle="modal"
+                        data-target="#newInvoiceItemModal">
+                        <i class="fas fa-plus me-2"></i> Add New Item
+                    </button>
+
+                </div>
+            </div>
+
+            <table class="table">
+                <thead class="table-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Service</th>
+                        <th scope="col" class="text-center">Unit Cost</th>
+                        <th scope="col" class="text-center">Quantity</th>
+                        <th scope="col">Details</th>
+                        <th scope="col" class="text-end">Total</th>
+                        <th scope="col" class="text-end">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    @foreach ($customerInvoice->customerInvoiceItems as $customerInvoiceItem)
+                    <tr class=""></tr>
+                    <td scope="row">{{ $loop->iteration }}</td>
+                    <td>{{ $customerInvoiceItem->service->service_name }}</td>
+                    <td class="text-center">{{ Number::format($customerInvoiceItem->unit_cost, 2) }}</td>
+                    <td class="text-center">{{ $customerInvoiceItem->quantity }}</td>
+                    <td>{{ $customerInvoiceItem->details }}</td>
+                    <td class="text-end">{{ $customerInvoiceItem->total }}</td>
+                    <td class="text-end">
+                        <a href="" class="text-danger">Remove</a>
+                    </td>
+                    </tr>
+                    @endforeach
+
+                    <tr class="d-none">
+                        <td colspan="4"></td>
+                        <td class="text-end">Sub-Total</td>
+                        <td class="text-end">{{ $customerInvoice->sub_total }}</td>
+                        <td></td>
+                    </tr>
+                    <tr class="d-none">
+                        <td colspan="4"></td>
+                        <td class="text-end">NHIL</td>
+                        <td class="text-end">{{ $customerInvoice->nhil_amount }}</td>
+                        <td></td>
+                    </tr>
+                    <tr class="d-none">
+                        <td colspan="4"></td>
+                        <td class="text-end">GetFund</td>
+                        <td class="text-end">{{ $customerInvoice->getfund_amount }}</td>
+                        <td></td>
+                    </tr>
+                    <tr class="d-none">
+                        <td colspan="4"></td>
+                        <td class="text-end">Value Added Tax </td>
+                        <td class="text-end">{{ $customerInvoice->vat_amount     }}</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td colspan="4"></td>
+                        <td class="text-end">Grand Total</td>
+                        <td class="text-end">{{ $customerInvoice->invoice_total }}</td>
+                        <td></td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+    <div class="card-footer bg-white d-flex ">
+        <button class="btn btn-danger ms-auto me-3">
+            Trash Invoice
+        </button>
+        <form action="{{ route('invoices.checkout-customer-invoice', $customerInvoice) }}" method="POST">
+            @csrf
+            <button class="btn btn-primary" type="submit">
+                Checkout Invoice
+                <i class="fa fa-long-arrow-alt-right ms-3"></i>
+            </button>
+        </form>
+
+    </div>
+</div>
+
+
+
+
+
+@include('app.invoices.modals.new-invoice-item-modal')
+
+@endsection
+
+@push('stacked-scripts')
+
+    @production
+        @vite(['resources/js/modules/invoices/print-service-calculator.js'])
+    @else
+            @vite(['resources/js/modules/invoices/print-service-calculator.js'])
+    @endproduction
+
+@endpush
