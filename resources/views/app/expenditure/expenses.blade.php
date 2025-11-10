c960123a67f28dc4a326c42486c9741a82d09d04
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2025-11-10 12:41:57 +0000

    refactor(expenses): improve layout and structure of expenses view

--- resources/views/app/expenditure/expenses.blade.php
+++ resources/views/app/expenditure/expenses.blade.php
@@ -2,148 +2,138 @@
 
 @section('content')
 
-    <x-printforce.headers.page-header title="Expenses">
-    <button
-                type="button"
-                class="btn btn-primary"
-                data-toggle="modal"
-                data-target="#newExpenditureModal">
+        <x-printforce.headers.page-header title="Expenses">
+        <button
+                    type="button"
+                    class="btn btn-primary"
+                    data-toggle="modal"
+                    data-target="#newExpenditureModal">
 
-                <i class="fas fa-plus me-3"></i>
-                New Expense
+                    <i class="fas fa-plus me-3"></i>
+                    New Expense
 
-            </button>
-    </x-printforce.headers.page-header>
-
-
-
-    <!-- Only show to admins -->
-    <div class="row mb-5">
-
-        <div class="col-md-2">
-            <x-printforce.cards.colour-card
-                title="Total Expenses"
-                :value="$total_expenditure"
-                bgColour="primary"
-                />
-        </div>
-
-        <div class="col-md-2">
-            <x-printforce.cards.colour-card
-                title="Monthly Expenses"
-                :value="$monthly_expenditure"
-                bgColour="danger"
-                />
-        </div>
-        <div class="col-md-2">
-            <x-printforce.cards.colour-card
-                title="Yearly Expenses"
-                :value="$yearly_expenditure"
-                bgColour="success"
-                />
-        </div>
-        <div class="col-md-2">
-
-        </div>
-
-    </div>
+                </button>
+        </x-printforce.headers.page-header>
 
 
-    <div class="card border-0">
-        <div class="card-body">
+        @can('administrator')
+            <!-- Only show to admins -->
+            <div class="row mb-5">
 
-            <!-- Only show to admin -->
-            @include('app.expenditure.partials.filter-expenses-frm')
+                <div class="col-md-2">
+                    <x-printforce.cards.colour-card title="Total Expenses" :value="$total_expenditure" bgColour="primary" />
+                </div>
+
+                <div class="col-md-2">
+                    <x-printforce.cards.colour-card title="Monthly Expenses" :value="$monthly_expenditure" bgColour="danger" />
+                </div>
+                <div class="col-md-2">
+                    <x-printforce.cards.colour-card title="Yearly Expenses" :value="$yearly_expenditure" bgColour="success" />
+                </div>
+                <div class="col-md-2">
+
+                </div>
+
+            </div>
+        @endcan
 
 
 
-            <div id="data_holder">
-                <table class="table datatables table-sm">
-                    <thead class="">
-                        <tr>
-                            <th>#</th>
-                            <th>Date</th>
-                            <th>Header</th>
-                            <th>Description</th>
-                            <th>Account</th>
-                            <th class="text-end">Amount</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @php
+        <div class="card border-0">
+            <div class="card-body">
+
+                <!-- Only show to admin -->
+                @include('app.expenditure.partials.filter-expenses-frm')
+
+
+
+                <div id="data_holder">
+                    <table class="table datatables table-sm">
+                        <thead class="">
+                            <tr>
+                                <th>#</th>
+                                <th>Date</th>
+                                <th>Header</th>
+                                <th>Description</th>
+                                <th>Account</th>
+                                <th class="text-end">Amount</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @php
     $i = 1;
     $total = 0;
-                        @endphp
+                            @endphp
 
-                        @foreach ($all_expenses as $expenditure)
+                            @foreach ($all_expenses as $expenditure)
 
-                                            @php
-                            $expenditure_id = $expenditure->expenditure_id
-                                            @endphp
+                                                @php
+        $expenditure_id = $expenditure->expenditure_id
+                                                @endphp
 
-                                            <tr class="">
-                                                <td>{{ $i++ }}</td>
-                                                <td>{{ $expenditure->date }}</td>
-                                                <td>{{ $expenditure->header?->header_name }}</td>
-                                                <td>
-                                                    {{ $expenditure->description }}
-                                                </td>
-                                                <td>{{ $expenditure->account->account_name }}</td>
-                                                <td class="text-end pe-20px">{{ number_format($expenditure->amount, 2) }}</td>
-                                                <td class="text-end col-2">
+                                                <tr class="">
+                                                    <td>{{ $i++ }}</td>
+                                                    <td>{{ $expenditure->date }}</td>
+                                                    <td>{{ $expenditure->header?->header_name }}</td>
+                                                    <td>
+                                                        {{ $expenditure->description }}
+                                                    </td>
+                                                    <td>{{ $expenditure->account->account_name }}</td>
+                                                    <td class="text-end pe-20px">{{ number_format($expenditure->amount, 2) }}</td>
+                                                    <td class="text-end col-2">
 
-                                                    <a href="javascript:void(0)" data-url="{{ route('accounting.expenditure.edit', $expenditure) }}" class="me-1 edit text-primary">
+                                                        <a href="javascript:void(0)" data-url="{{ route('accounting.expenditure.edit', $expenditure) }}" class="me-1 edit text-primary">
 
-                                                        <i class="fas fa-pen"></i>
-                                                        Edit
+                                                            <i class="fas fa-pen"></i>
+                                                            Edit
 
-                                                    </a>
+                                                        </a>
 
-                                                <form id="deleteFrm" method="POST" action="{{ route('accounting.expenditure.delete', $expenditure) }}" class="d-inline-block">
-                                                    @csrf
-                                                    <a href="javascript:void(0)" class="delete text-danger" data-url="">
+                                                    <form id="deleteFrm" method="POST" action="{{ route('accounting.expenditure.delete', $expenditure) }}" class="d-inline-block">
+                                                        @csrf
+                                                        <a href="javascript:void(0)" class="delete text-danger" data-url="">
 
-                                                        <i class="fas fa-trash-alt text-danger"></i>
-                                                        Delete
+                                                            <i class="fas fa-trash-alt text-danger"></i>
+                                                            Delete
 
-                                                    </a>
-                                                </form>
+                                                        </a>
+                                                    </form>
 
 
-                                                </td>
-                                            </tr>
+                                                    </td>
+                                                </tr>
 
-                                            @php
-                            $total += $expenditure->amount; //increment total by amount
-                                            @endphp
+                                                @php
+        $total += $expenditure->amount; //increment total by amount
+                                                @endphp
 
-                        @endforeach
+                            @endforeach
 
-                        <tr>
-                            <td>-----------</td>
-                            <td>-----------</td>
-                            <td>-----------</td>
-                            <td>-----------</td>
-                            <td>-----------</td>
-                            <td class="text-end Axiforma fs-20px fw-600 pe-20px">
-                                {{ number_format($total, 2) }}
-                            </td>
-                            <td></td>
-                        </tr>
-                    </tbody>
-                </table>
+                            <tr>
+                                <td>-----------</td>
+                                <td>-----------</td>
+                                <td>-----------</td>
+                                <td>-----------</td>
+                                <td>-----------</td>
+                                <td class="text-end Axiforma fs-20px fw-600 pe-20px">
+                                    {{ number_format($total, 2) }}
+                                </td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
             </div>
-
         </div>
-    </div>
 
 
 
-    <!-- include new expenditure modal -->
-    @include('app.expenditure.modals.new-expense-modal')
+        <!-- include new expenditure modal -->
+        @include('app.expenditure.modals.new-expense-modal')
 
-    <div id="modal_holder"></div>
+        <div id="modal_holder"></div>
 @endsection
 
 @section('js')
