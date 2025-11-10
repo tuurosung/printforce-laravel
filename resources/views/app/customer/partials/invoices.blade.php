c418fccf64374cbb7c28a83884cbb927af76b59a
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2025-11-10 12:42:47 +0000

    refactor(invoices): update layout and structure for improved readability and consistency

--- resources/views/app/customer/partials/invoices.blade.php
+++ resources/views/app/customer/partials/invoices.blade.php
@@ -1,46 +1,46 @@
 <h4 class="Axiforma mb-3">Invoices</h4>
 
-<table class="table table-secondary ">
+<table class="table table-sm datatables">
     <thead class="">
         <tr>
             <th>#</th>
+            <th>Date Created</th>
             <th>Invoice ID</th>
             <th>Customer Name</th>
-            <th>P.O. #</th>
-            <th class="text-right">Sub-Total</th>
-            <th class="text-right">Taxes</th>
-            <th class="text-right">Total</th>
+            <th class="text-end">Sub-Total</th>
+            <th class="text-end">Taxes</th>
+            <th class="text-end">Total</th>
             <th></th>
         </tr>
     </thead>
     <tbody>
 
-        @foreach ($customer->invoices as $invoices)
-
-
-
-        <tr>
-            <td><?php echo $i++; ?></td>
-            <td><?php echo $invoices['invoice_id']; ?></td>
-            <td><?php echo $customer?->name; ?></td>
-            <td><?php echo $invoices['ref']; ?></td>
-            <td class="text-right"><?php echo $invoices['sub_total']; ?></td>
-            <td class="text-right"><?php echo number_format($invoices['vat_amount'] + $invoices['nhil_amount'] + $invoices['getfund_amount'], 2); ?></td>
-            <td class="text-right"><?php echo $invoices['total']; ?></td>
-            <td class="text-right">
-                <div class="dropdown open">
-                    <button class="btn btn-primary btn-sm dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Option
-                    </button>
-                    <div class="dropdown-menu b-0 p-0" aria-labelledby="dropdownMenu1">
-                        <ul class="list-group">
-                            <a class="list-group-item" href="invoice_prepare.php?invoice_id=<?php echo $invoices['invoice_id']; ?>">Edit</li>
-                                <a class="list-group-item" href="customer_portal.php?customer_id=<?php echo $customers['customer_id']; ?>">Portal</a>
-                        </ul>
+        @foreach ($customer->invoices as $customerInvoice)
+            <tr>
+                <td>{{ $loop->iteration }}</td>
+                <td>{{ $customerInvoice->created_at }}</td>
+                <td>
+                    <a href="#">{{ $customerInvoice->invoice_id }}</a>
+                </td>
+                <td>{{ $customerInvoice->customer->name }}</td>
+                <td class="text-end">{{ number_format($customerInvoice->sub_total, 2) }}</td>
+                <td class="text-end">
+                    {{ number_format($customerInvoice->vat_amount + $customerInvoice->nhil_amount + $customerInvoice->getfund_amount, 2) }}
+                </td>
+                <td class="text-end">{{ number_format($customerInvoice->total, 2) }}</td>
+                <td class="text-end">
+                    <div class="dropdown">
+                        <a class="dropdown-toggle" type="button" id="triggerId" data-bs-toggle="dropdown" aria-haspopup="true"
+                            aria-expanded="false">
+                            Options
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="triggerId">
+                            <a class="dropdown-item" href="#">Edit</a>
+                            <a class="dropdown-item" href="#">Delete</a>
+                        </div>
                     </div>
-                </div>
-            </td>
-        </tr>
+                </td>
+            </tr>
 
         @endforeach
 
