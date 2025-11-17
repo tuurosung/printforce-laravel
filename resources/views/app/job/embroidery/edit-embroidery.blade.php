8a77838b8bfa6cf266b3009cd3ef363784cefd3f
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2025-11-17 02:28:49 +0000

    feat(EditEmbroideryJobModal): refactor modal structure and update form fields for better clarity

--- resources/views/app/job/embroidery/edit-embroidery.blade.php
+++ resources/views/app/job/embroidery/edit-embroidery.blade.php
@@ -1,6 +1,6 @@
-<div class="modal fade" id="edit_embroidery_job_modal" tabindex="-1" role="dialog" aria-labelledby="modalTitleId"
+<div class="modal fade" id="editEmbroideryJobModal" tabindex="-1" role="dialog" aria-labelledby="modalTitleId"
     aria-hidden="true">
-    <div class="modal-dialog modal-xl" role="document">
+    <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="modalTitleId">
@@ -13,91 +13,127 @@
 
                 <div class="modal-body">
 
-                    <div class="row">
-                        <div class="col">
-                            <div class="card card-primary border-primary mb-4">
-                                <div class="card-body">
+                    <section class="mb-4">
 
-                                    <h6 class="mb-4">Job Details </h6>
+                        <h6 class="mb-3">Customer Details</h6>
 
-                                    <div class="row">
-                                        <div class="col">
-
-                                            <x-printforce.inputs.select-input name="service_id" id="em_services"
-                                                label="Service Name" :options="$embroidery_services"
-                                                :selected="$embroideryJob->service_id" required />
-
-
-                                        </div>
-                                        <div class="col-md-6">
-                                            <x-printforce.inputs.text-input name="unit_cost" id="embroidery_unit_cost"
-                                                label="Unit Cost" placeholder="0.00" :value="$embroideryJob->unit_cost" readonly="true" />
-                                        </div>
-                                    </div>
-
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <x-printforce.inputs.text-input name="qty" id="embroidery_qty"
-                                                label="Quantity" placeholder="1" :value="$embroideryJob->qty" required />
-                                        </div>
-                                        <div class="col-md-6">
-                                            <x-printforce.inputs.text-input name="embroidery_cost" id="embroidery_cost"
-                                                label="Embroidery Cost" placeholder="0.00" :value="$embroideryJob->embroidery_cost" readonly="true" />
-                                        </div>
-                                    </div>
-
-                                    <x-printforce.inputs.text-input name="notes" id="em_notes"
-                                        label="Notes (optional eg; CRS Farmers T-shirts)" placeholder="" :value="$embroideryJob->notes" />
-
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label for="customer_name" class="form-label">Customer Name</label>
+                                    <input type="text" class="form-control" name="customer_name" id="customer_name"
+                                        readonly value="{{ $embroideryJob->customer->name }}" />
                                 </div>
+
+                            </div>
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label for="date" class="form-label">Date</label>
+                                    <input type="text" class="form-control" name="date" id="date"
+                                        aria-describedby="helpId" placeholder="" value="{{ $embroideryJob->date }}" />
+                                </div>
+
                             </div>
                         </div>
 
-                        <div class="col">
 
-                            <div class="card card-warning border-warning">
-                                <div class="card-body">
+                    </section>
 
-                                    <h6 class="mb-4">Production Details</h6>
+                    <section class="mb-4">
 
-                                    <div class="row">
-                                        <div class="col-6">
-                                            <div class="mb-3">
-                                                <label for="" class="form-label">Material Purchase</label>
-                                                <select class="form-select form-select-sm" name="mat_supply"
-                                                    id="mat_supply">
-                                                    <option value="">-- Who is buying the materials? --</option>
-                                                    @foreach (\App\Services\Jobs\EmbroideryJobService::materialSupplyOptions() as $key => $value)
-                                                           <option value="{{ $key }}" {{ $embroideryJob->mat_supply == $key ? 'selected' : '' }}>{{ $value }}</option>
-                                                    @endforeach
-                                                </select>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <x-printforce.inputs.text-input name="mat_unit_cost" id="mat_unit_cost"
-                                                label="Material Unit Cost" placeholder="0.00" value="{{ $embroideryJob->mat_unit_cost }}" required />
-                                        </div>
+                        <h6 class="mb-3">Job Details</h6>
+
+                        <div class="row">
+                            <div class="col-md-6">
+
+                                <div class="mb-3">
+                                    <label for="service_id" class="form-label">Service Name</label>
+                                    <select class="form-select" name="service_id" id="service_id"
+                                        data-customer_id="{{ $embroideryJob->customer_id }}"
+                                        data-fetch-url="{{ route('configuration.print-services.get-service-cost-with-customer-id') }}"
+                                        required>
+                                        <option value="">Select Option</option>
+                                        @foreach ($embroidery_services as $key => $value)
+                                        <option value="{{ $key }}"
+                                            {{ $embroideryJob->service_id === $key ? 'selected' : '' }}>
+                                            {{ $value }}
+                                        </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+
+                            </div>
+                            <div class="col-md-6">
+                                <x-printforce.inputs.text-input name="unit_cost" id="embroidery_unit_cost"
+                                    label="Unit Cost" placeholder="0.00" :value="$embroideryJob->unit_cost"
+                                    readonly="true" />
+                            </div>
+                        </div>
+                    </section>
+
+                    <section class="mb-4">
+
+                        <h6 class="mb-3">Production Details</h6>
+
+                        <div class="p-3 bg-light rounded-3">
+
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <x-printforce.inputs.text-input name="qty" id="embroidery_qty" label="Quantity"
+                                        placeholder="1" :value="$embroideryJob->qty" required />
+                                </div>
+                                <div class="col-md-6">
+                                    <x-printforce.inputs.text-input name="embroidery_cost" id="embroidery_cost"
+                                        label="Embroidery Cost" placeholder="0.00"
+                                        :value="$embroideryJob->embroidery_cost" readonly="true" />
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-6">
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Material Purchase</label>
+                                        <select class="form-select form-select-sm" name="mat_supply" id="mat_supply">
+                                            <option value="">-- Who is buying the materials? --</option>
+                                            @foreach(config('printforce.printServices.material_supply_options') as $key => $value)
+                                            <option value="{{ $key }}"
+                                                {{ $embroideryJob->mat_supply == $key ? 'selected' : '' }}>
+                                                {{ $value }}</option>
+                                            @endforeach
+                                        </select>
                                     </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <x-printforce.inputs.text-input name="mat_unit_cost" id="mat_unit_cost"
+                                        label="Material Unit Cost" placeholder="0.00"
+                                        value="{{ $embroideryJob->mat_unit_cost }}" required />
+                                </div>
+                            </div>
 
-
-                                    <div class="row">
-                                        <div class="col-6">
-                                            <x-printforce.inputs.text-input name="purchase_cost"
-                                                id="embroidery_purchase_cost" label="Material Purchase Cost"
-                                                placeholder="0.00" value="{{ $embroideryJob->purchase_cost }}" readonly required />
-                                        </div>
-                                        <div class="col-md-6">
-                                            <x-printforce.inputs.text-input name="total" id="embroidery_total"
-                                                label="Overall Cost" value="{{ $embroideryJob->total }}" placeholder="0.00" readonly />
-                                        </div>
-                                    </div>
-
+                            <div class="row">
+                                <div class="col-6">
+                                    <x-printforce.inputs.text-input name="purchase_cost"
+                                        id="embroidery_purchase_cost" label="Material Purchase Cost"
+                                        placeholder="0.00" value="{{ $embroideryJob->purchase_cost }}" readonly
+                                        required />
+                                </div>
+                                <div class="col-md-6">
+                                    <x-printforce.inputs.text-input name="total" id="embroidery_total"
+                                        label="Overall Cost" value="{{ $embroideryJob->total }}"
+                                        placeholder="0.00" readonly />
                                 </div>
                             </div>
 
                         </div>
-                    </div>
 
+                    </section>
+
+                    <section class="mb-4">
+                        <h6 class="mb-3">Additional Details</h6>
+                        <x-printforce.inputs.text-input name="notes" id="em_notes"
+                                        label="Notes (optional eg; CRS Farmers T-shirts)" placeholder=""
+                                        :value="$embroideryJob->notes" />
+                    </section>
 
                 </div>
                 <div class="modal-footer">
@@ -106,7 +142,7 @@
                     </button>
                     <button type="submit" class="btn btn-primary">
                         <i class="fas fa-check me-3  "></i>
-                        Create Embroidery Job
+                        Update Embroidery Job
                     </button>
                 </div>
             </form>
