9f2cf3160ee4c6498dc7787fddd7d4b73baf9da7
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2025-11-18 02:18:28 +0000

    feat(FetchEmbroideryJobsController): add controller to fetch and display recent embroidery jobs

---
+++ app/Http/Controllers/Jobs/Embroidery/FetchEmbroideryJobsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Jobs\Embroidery;
+
+use App\Http\Controllers\Controller;
+use App\Services\Jobs\EmbroideryJobService;
+use Illuminate\Http\Request;
+
+class FetchEmbroideryJobsController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        return view('app.job.embroidery.data.load-embroideryjobs', [
+            'jobs' => (new EmbroideryJobService())->getRecentJobs(),
+        ]);
+    }
+}
