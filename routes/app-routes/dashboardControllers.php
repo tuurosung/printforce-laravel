a1512c570e42caed480e4f02039bc7b7448b88c8
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2025-11-19 22:28:46 +0000

    refactor: remove DashboardController, JobService, dashboard view, and related routes

--- routes/app-routes/dashboardControllers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
-
-Route::prefix('/dashboard')->controller(DashboardController::class)->group(function () {
-    Route::get('/', 'index')->name('dashboard');
-});
