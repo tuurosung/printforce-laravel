154747d82460370d4ff38303f3f3a71115d88e04
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2025-11-19 08:37:26 +0000

    feat(DashboardController): implement dashboard functionality with customer and payment analytics

---
+++ app/Http/Controllers/Dashboards/DashboardController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Dashboards;
+
+use Illuminate\Http\Request;
+use App\Services\CustomerService;
+use App\Models\Customers\Customer;
+use App\Http\Controllers\Controller;
+use App\Services\ExpenditureService;
+use App\Services\CustomerPaymentService;
+use App\Models\Customers\CustomerPayment;
+use App\Http\Controllers\Jobs\JobController;
+use App\Services\Accounting\AccountService;
+use App\Services\Jobs\CustomerJobService;
+
+class DashboardController extends Controller
+{
+
+    public function __construct(
+        private CustomerService $customerService,
+        private CustomerPaymentService $customerPaymentService,
+        private ExpenditureService $expenditureService,
+        private AccountService $accountService,
+    ){}
+
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $countNewCustomers = Customer::countNewCustomers();
+        // $debtorsList = $customer->debtorsList();
+
+
+        $service_performance = JobController::servicePerformanceAnalytics();
+        $customer_rankings = CustomerService::getCustomerRanking();
+
+        $payment_graph = CustomerPayment::paymentGraph();
+
+        $expenditure_statistics = $this->expenditureService->statistics;
+
+
+        return view('app.dashboard.dashboard', [
+            'todays_payments' => $this->customerPaymentService->getTodaysPaymentTotal(),
+            'monthly_payments' => $this->customerPaymentService->getMonthlyPaymentTotal(),
+            'monthly_expenditure' => $expenditure_statistics['monthly_expenditure'],
+            'countNewCustomers' => $countNewCustomers,
+            'customer_rankings' => $customer_rankings,
+            'service_performance' => $service_performance,
+            'payment_graph' => $payment_graph,
+
+            'todays_jobs_count' => CustomerJobService::countTodaysJobs(),
+
+            'payment_accounts' => $this->accountService->getAssetAccountList(),
+        ]);
+    }
+}
