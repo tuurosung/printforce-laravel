5cb28a9afc4a645e0947fa3a6a2c3af03895e611
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2025-11-24 01:36:45 +0000

    refactor(app): comment out jQuery and related imports for clarity and organization

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,34 +1,56 @@
-import $ from "jquery";
+// // Import jQuery and make it globally available FIRST
+// import $ from 'jquery';
 
-window.$ = window.jQuery = $;
+// // Set jQuery on window object BEFORE any other imports that need it
+// window.$ = $;
+// window.jQuery = $;
 
-import '@popperjs/core';
+// // Set globalThis for modules that might use it
+// if (typeof globalThis !== 'undefined') {
+//     globalThis.jQuery = $;
+//     globalThis.$ = $;
+// }
 
-import './printforce/jquery.js';
-import './printforce/popper.min.js';
-import './printforce/bootbox.js';
-import '../plugins/datatables/datatables.min.js';
-import './printforce/bootstrap-datepicker.js';
+// $.fn.modal.Constructor.prototype._enforceFocus = function () { };
 
-import './printforce/app.js';
-import './printforce/app.min.js';
-import './printforce/bootstrap.min.js';
-import './printforce/bootstrap-modal.js';
-import './printforce/modules/chart.js';
-import './printforce/toastify.min.js';
-import './printforce/lity.min.js';
+// Import Bootstrap (depends on jQuery)
+// import 'bootstrap';
+
+// // Import DataTables
+// import 'datatables.net-bs5';
+
+// // Import Bootstrap Datepicker
+// import 'bootstrap-datepicker';
+
+// import './clean-footer.js';
+// import './handle-datatables.js';
+// import './handle-datepickers.js';
+// import './handle-tab-states.js';
+
+// // Import Select2
+// // import 'select2';
+
+// // Import bootbox - now jQuery should be available
+
+// import './printforce/app.js';
+// import './printforce/app.min.js';
+// import './printforce/bootstrap-modal.js';
+// import './printforce/modules/chart.js';
+// import './printforce/toastify.min.js';
+// import './printforce/lity.min.js';
+// import './printforce/bootbox.js';
+// import './printforce/modules/perfect-scrollbar.min.js';
 
 
-import './matdash/bootstrap.bundle.min.js';
-import './matdash/simplebar.min.js';
-import './matdash/app.init.js';
-import './matdash/theme.js';
-import './matdash/app.min.js';
+// // Import matdash scripts
+// import './matdash/simplebar.min.js';
+// import './matdash/app.init.js';
+// import './matdash/theme.js';
+// import './matdash/app.min.js';
 
-import './matdash/apexcharts.min.js';
-import './matdash/homepage.js';
-import './matdash/iconify-icon.min.js';
-import './matdash/owl.carousel.min.js';
+// import './matdash/apexcharts.min.js';
+// import './matdash/homepage.js';
+// import './matdash/iconify-icon.min.js';
+// import './matdash/owl.carousel.min.js';
 
-
-import './matdash/vendor.min.js';
+// import './matdash/vendor.min.js';
