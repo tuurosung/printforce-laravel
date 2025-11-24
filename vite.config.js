7e54695baa40f7d9d061c786e5a34df55b90f69b
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2025-11-24 01:40:46 +0000

    feat(dependencies): add new PHP packages for enhanced functionality

--- vite.config.js
+++ vite.config.js
@@ -9,18 +9,19 @@ export default defineConfig({
                 'resources/js/app.js',
 
                 // core modules
-                'resources/js/modules/initializeDataTables.js',
+                // 'resources/js/modules/initializeDataTables.js',
+                // 'resources/js/modules/initializeDatepickers.js',
 
-                // jobs - large format
-                'resources/js/modules/jobs/largeformat/largeformat-jobs-scripts.js',
-                'resources/js/modules/jobs/largeformat/CalculateLargeFormatTotal.js',
-                'resources/js/modules/jobs/largeformat/GetLargeFormatjobsData.js',
-                'resources/js/modules/jobs/embroidery/embroidery-job-scripts.js',
+                // // jobs - large format
+                // 'resources/js/modules/jobs/largeformat/largeformat-jobs-scripts.js',
+                // 'resources/js/modules/jobs/largeformat/CalculateLargeFormatTotal.js',
+                // 'resources/js/modules/jobs/largeformat/GetLargeFormatjobsData.js',
+                // 'resources/js/modules/jobs/embroidery/embroidery-job-scripts.js',
 
-                // invoices
-                'resources/js/modules/invoices/print-service-calculator.js'
+                // // invoices
+                // 'resources/js/modules/invoices/print-service-calculator.js'
             ],
             refresh: true,
-        }),``
+        }),
     ],
 });
