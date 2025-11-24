1cddf3b64f33369b0a8f08fe4c56ec2bc19520d3
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2025-11-24 01:31:20 +0000

    feat(GetEmbroideryJobsData): implement module for fetching and displaying embroidery job data

---
+++ public/js/modules/jobs/embroidery/GetEmbroideryJobsData.js
@@ -0,0 +1,42 @@
+import { InitializeDataTables } from "../../initializeDataTables";
+
+
+export const GetEmbroideryJobsData = {
+
+    config: {
+        URL: window.routes.getEmbroideryJobs
+    },
+
+    elements: {},
+
+    init: function() {
+        this.cachedElements();
+        this.getData();
+    },
+
+    cachedElements: function() {
+        this.elements = {
+            $dataHolder: $('#data_holder'),
+        }
+    },
+
+    async getData() {
+
+        try {
+
+            const response = await $.get(this.config.URL);
+            this.elements.$dataHolder.html(response);
+            InitializeDataTables.init();
+
+        } catch (error) {
+            console.error('Error refreshing data table:', error);
+        }
+
+    }
+
+
+
+
+}
+
+$(document).ready(() => GetEmbroideryJobsData.init());
