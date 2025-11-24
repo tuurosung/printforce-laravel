e2d00cd561c5a790c4c735917d094d5055998580
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2025-11-24 01:25:00 +0000

    feat(data-tables): add InitializeDataTables module for DataTable initialization

---
+++ public/js/modules/initializeDataTables.js
@@ -0,0 +1,29 @@
+
+export const InitializeDataTables = {
+
+    init: function() {
+        this.cachedElements();
+        this.initializeTables();
+    },
+
+    elements: {},
+
+    cachedElements: function() {
+        this.elements = {
+            $dataTables: $('.datatables'),
+        };
+    },
+
+
+    initializeTables: function() {
+
+        this.elements.$dataTables.each(function() {
+            $(this).DataTable();
+        });
+
+    },
+}
+
+
+$(document).ready(() => InitializeDataTables.init());
+window.InitializeDataTables = InitializeDataTables;
