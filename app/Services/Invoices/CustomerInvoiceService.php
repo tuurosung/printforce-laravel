17899a370cc8d3d7f0a253a03b01f5e82dd8a5ad
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-07 06:58:09 +0000

    refactor(invoice): enhance CustomerInvoiceService by improving method structure and organization

--- app/Services/Invoices/CustomerInvoiceService.php
+++ app/Services/Invoices/CustomerInvoiceService.php
@@ -2,35 +2,82 @@
 
 namespace App\Services\Invoices;
 
-use Illuminate\Support\Facades\Session;
+use App\Abstracts\Invoices\BaseInvoiceService;
 use App\Models\Invoices\CustomerInvoice;
+use App\Models\Invoices\CustomerInvoiceItem;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
+use Override;
 
-class CustomerInvoiceService
+class CustomerInvoiceService extends BaseInvoiceService
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
+    public function createInvoice(array $data): CustomerInvoice
     {
-        //
+        return DB::transaction(function () use ($data) {
+            return CustomerInvoice::create($data);
+        });
     }
 
 
-    public static function generateInvoiceNumber()
+    #[Override]
+    public function getInvoices(array $filters = []): Collection
     {
-        return generatedNumericId(10);
+        return CustomerInvoice::query()
+            ->get();
     }
 
 
-    public static function checkoutCustomerInvoice(CustomerInvoice $customerInvoice)
+    public function addItem(CustomerInvoice $invoice, array $data): bool
     {
-        // Logic to finalize the checkout process for the customer invoice
-        $customerInvoice->status = 'active';
+        $item = CustomerInvoiceItem::create([
+            'invoice_id' => $invoice->invoice_id,
+            ...$data
+        ]);
 
-        $customerInvoice->updateSubTotal();
-        $customerInvoice->save();
+        if ($item) {
+            $this->recalculateTotals($invoice);
+        }
 
-        // destroy session data related to the invoice
-        Session::forget('active_customer_invoice');
+        return true;
+    }
+
+
+    public function checkout(CustomerInvoice $invoice): void
+    {
+        DB::transaction(function () use ($invoice) {
+            $this->recalculateTotals($invoice);
+            $invoice->update([
+                'status' => 'active'
+            ]);
+
+            $this->clearActiveInvoiceSession();
+        });
+    }
+
+
+    public function markAsPaid(CustomerInvoice $invoice): void
+    {
+        $invoice->update([
+            'status' => 'paid',
+            'paid_at' => now()
+        ]);
+    }
+
+
+    public function cancel(CustomerInvoice $invoice): void
+    {
+        $invoice->update([
+            'status' => 'cancelled',
+            'cancelled_at' => now()
+        ]);
+
+        $this->clearActiveInvoiceSession();
+    }
+
+
+    protected function applyCharges(int $subTotal, CustomerInvoice $invoice): int
+    {
+        return $subTotal;
     }
 }
