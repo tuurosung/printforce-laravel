c79b4e0fb1059fb3064262b15ca9e0bec27c47dc
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-07 15:30:03 +0000

    feat(customer): add CustomerRepositoryInterface for customer management operations

---
+++ app/Domain/Customers/Contracts/CustomerRepositoryInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Domain\Customers\Contracts;
+
+use App\Data\CustomerData;
+use App\Models\Customers\Customer;
+use Illuminate\Database\Eloquent\Collection;
+
+interface CustomerRepositoryInterface
+{
+
+    // CRUD Operations
+    public function createCustomer(CustomerData $data): Customer;
+    public function updateCustomer(Customer $customer, CustomerData $data): bool;
+    public function deleteCustomer(Customer $customer): bool;
+
+
+    // lifecycle
+    public function getLatestCustomers(): Collection;
+    public function filterCustomers(string $searchTerm): Collection;
+    public function getAllCustomers(): Collection;
+    public function getCustomersArray(): array;
+    public function getCustomerRanking(): Collection;
+    public static function getCustomerStatistics(): array;
+}
