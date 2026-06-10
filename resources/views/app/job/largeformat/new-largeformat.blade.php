2aa274f6b7ddce340720a2c74a64ccb6b57c0a19
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-10 04:04:54 +0000

    Refactor new large format job modal to improve structure and styling

--- resources/views/app/job/largeformat/new-largeformat.blade.php
+++ resources/views/app/job/largeformat/new-largeformat.blade.php
@@ -1,192 +1,167 @@
-<div class="modal fade" id="new_largeformat_modal" tabindex="-1" role="dialog" aria-labelledby="modalTitleId"
-    aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="modalTitleId">
+<div id="large_format_job_modal"
+    class="hs-overlay hs-overlay-open:opacity-100 hs-overlay-open:duration-500 hidden size-full fixed top-0 start-0 z-[80] opacity-0 overflow-x-hidden transition-all overflow-y-auto pointer-events-none">
+    <div
+        class="hs-overlay-open:opacity-100 hs-overlay-open:duration-500 opacity-0 transition-all lg:max-w-3xl lg:w-full m-3 lg:mx-auto">
+        <div
+            class="flex flex-col bg-white border shadow-sm rounded-md pointer-events-auto dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]">
+            <div class="flex justify-between items-center py-3 px-6 border-b border-border dark:border-gray-700">
+
+                <h3 class="text-base font-cal-sans-regular text-gray-800 dark:text-white">
                     New Large Format Job
-                </h5>
-                <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+                </h3>
+
+                <button type="button"
+                    class="flex justify-center items-center size-7 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                    data-hs-overlay="#large_format_job_modal">
+                    <span class="sr-only">Close</span>
+                    <i class="ti ti-x text-xl"></i>
+                </button>
+
             </div>
             <form method="POST" action="{{ route('jobs.largeformat.store', $customer) }}">
                 @csrf
+                <div class="p-6 overflow-y-auto">
 
-                <div class="modal-body">
 
-                    <section class="mb-4">
-                        <h6 class="mb-3">Job Details</h6>
+                    <div class="modal-body">
 
-                        <div class="row">
-                            <div class="col-md-6">
+                        <section class="mb-8">
+
+
+                            <div class="grid grid-cols-2 gap-6 mb-4">
                                 <div class="mb-3">
                                     <label for="customer_name" class="form-label">Customer Name</label>
-                                    <input type="text" class="form-control" name="customer_name" id="customer_name"
+                                    <input type="text" class="form-control py-2.5 px-4" name="customer_name" id="customer_name"
                                         aria-describedby="helpId" placeholder="" value="{{ $customer->name }}" readonly
                                         readonly />
                                 </div>
-
-                            </div>
-                            <div class="col-md-6">
                                 <div class="mb-3">
                                     <label for="date" class="form-label">Job Date</label>
-                                    <input type="text" class="form-control datepicker-input" name="date"
+                                    <input type="text" class="form-control datepicker-input py-2.5 px-4" name="date"
                                         id="largeformat_date" value="{{ now()->format('Y-m-d') }}" placeholder=""
+                                        required />
+                                </div>
+                            </div>
+
+
+                            <div class="grid grid-cols-2 gap-6">
+
+                                <div class="col">
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Service Name</label>
+                                        <select class="form-control py-2.5 px-4" name="service_id" id="largeformat_service_id"
+                                            data-fetch_url="{{ route('configuration.print-services.get-service-cost-with-customer-id') }}"
+                                            data-customer_id="{{ $customer->customer_id }}" required>
+
+                                            <option value="">--</option>
+
+                                            @foreach ($largeformat_services as $key => $value)
+                                            <option value="{{ $key }}">{{ $value }}</option>
+                                            @endforeach
+
+                                        </select>
+                                    </div>
+
+                                </div>
+
+                                <div class="col">
+                                    <div class="mb-3">
+                                        <label for="cost" class="form-label">Unit Cost</label>
+                                        <input type="text" class="form-control py-2.5 px-4" name="cost" id="largeformat_cost"
+                                            readonly required />
+                                    </div>
+
+                                </div>
+                            </div>
+                        </section>
+
+
+
+                        <section class="mb-8">
+                            <!-- <h6 class="mb-3">Dimension & Quantity</h6> -->
+
+                            <div class="grid grid-cols-4 gap-6">
+                                <div class="mb-3">
+                                    <label for="" class="form-label">Width</label>
+                                    <input type="number" step="any" class="form-control py-2.5 px-4" name="width" id="width"
+                                        required />
+                                </div>
+                                <div class="mb-3">
+                                    <label for="" class="form-label">Height</label>
+                                    <input type="number" step="any" class="form-control py-2.5 px-4" name="height" id="height" required />
+                                </div>
+                                <div class="mb-3">
+                                    <label for="" class="form-label">Unit Of Measurement</label>
+                                    <select class="form-control py-2.5 px-4" name="measuring_unit" id="measuring_unit" required>
+                                        <option value="ft">Feet</option>
+                                        <option value="inch">Inch</option>
+                                    </select>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="" class="form-label">Copies</label>
+                                    <input type="number" class="form-control py-2.5 px-4" name="copies" id="largeformat_copies" placeholder="Number of Copies"
+                                        min="1" value="1" required />
+                                </div>
+                            </div>
+
+                        </section>
+
+
+                        <div class="mb-6">
+
+                            <!-- <h6 class="mb-3">Dimensions & Pricing</h6> -->
+
+
+
+                            <div class="grid grid-cols-4 gap-6">
+
+                                <div class="mb-3">
+                                    <label for="" class="form-label">Sub - Total</label>
+                                    <input type="text" class="form-control py-2.5 px-4" name="sub_total" id="largeformat_sub_total"
+                                        readonly required />
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="" class="form-label">Discount</label>
+                                    <input type="number" step="any" class="form-control py-2.5 px-4" name="discount"
+                                        id="largeformat_discount" value="0" required>
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="" class="form-label">Premium</label>
+                                    <input type="number" step="any" class="form-control py-2.5 px-4" name="premium"
+                                        id="largeformat_premium" value="0" required>
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="" class="form-label">Total Cost</label>
+                                    <input type="text" class="form-control py-2.5 px-4" name="total" id="largeformat_total" readonly
                                         required />
                                 </div>
                             </div>
                         </div>
 
-                        <div class="row">
 
-                            <div class="col">
-                                <div class="mb-3">
-                                    <label for="" class="form-label">Service Name</label>
-                                    <select class="form-select select2-input" name="service_id"
-                                        id="largeformat_service_id"
-                                        data-fetch_url="{{ route('configuration.print-services.get-service-cost-with-customer-id') }}"
-                                        data-customer_id="{{ $customer->customer_id }}" required>
-
-                                        <option value="">--Select Service--</option>
-
-                                        @foreach ($largeformat_services as $key => $value)
-                                        <option value="{{ $key }}">{{ $value }}</option>
-                                        @endforeach
-
-                                    </select>
-                                </div>
-
-                            </div>
-
-                            <div class="col">
-                                <div class="mb-3">
-                                    <label for="cost" class="form-label">Unit Cost</label>
-                                    <input type="text" class="form-control form-control-sm" name="cost"
-                                        id="largeformat_cost" readonly required />
-                                </div>
-
-                            </div>
+                        <div class="mb-3">
+                            <label for="" class="form-label">Notes (optional eg; USAID Banner)</label>
+                            <input type="text" class="form-control  py-2.5 px-4" name="notes" id="lf_notes" value="" />
                         </div>
-                    </section>
 
-
-
-                    <section class="mb-3">
-                        <h6 class="mb-3">Dimension & Quantity</h6>
-
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="mb-3">
-                                            <label for="" class="form-label">Width</label>
-                                            <input type="number" step="any" class="form-control form-control-sm"
-                                                name="width" id="width" required />
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="mb-3">
-                                            <label for="" class="form-label">Height</label>
-                                            <input type="number" step="any" class="form-control form-control-sm"
-                                                name="height" id="height" required />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="mb-3">
-
-                                            <label for="" class="form-label">Unit Of Measurement</label>
-                                            <select class="form-select form-select-sm select2-input"
-                                                name="measuring_unit" id="measuring_unit" required>
-
-                                                <option value="ft">Feet</option>
-                                                <option value="inch">Inch</option>
-
-                                            </select>
-
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="mb-3">
-                                            <label for="" class="form-label">Copies</label>
-                                            <input type="number" class="form-control form-control-sm" name="copies"
-                                                id="largeformat_copies" placeholder="Number of Copies" min="1" value="1"
-                                                required />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-
-
-                    <section class="mb-3">
-
-                        <h6 class="mb-3">Dimensions & Pricing</h6>
-
-
-
-                        <div class="bg-light p-3 rounded-2">
-
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="mb-3">
-                                        <label for="" class="form-label">Sub - Total</label>
-                                        <input type="text" class="form-control form-control-sm" name="sub_total"
-                                            id="largeformat_sub_total" readonly required />
-                                    </div>
-
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="mb-3">
-                                        <label for="" class="form-label">Discount</label>
-                                        <input type="number" step="any" class="form-control form-control-sm"
-                                            name="discount" id="largeformat_discount" value="0" required>
-                                    </div>
-                                </div>
-                            </div>
-
-
-                            <div class="row">
-
-                                <div class="col">
-                                    <div class="mb-3">
-                                        <label for="" class="form-label">Premium</label>
-                                        <input type="number" step="any" class="form-control form-control-sm"
-                                            name="premium" id="largeformat_premium" value="0" required>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-6 mb-4 mb-md-0">
-                                    <div class="mb-3">
-                                        <label for="" class="form-label">Total Cost</label>
-                                        <input type="text" class="form-control form-control-sm" name="total"
-                                            id="largeformat_total" readonly required />
-                                    </div>
-                                </div>
-
-                            </div>
-
-                        </div>
-                    </section>
-
-
-                    <div class="mb-3">
-                        <label for="" class="form-label">Notes (optional eg; USAID Banner)</label>
-                        <input type="text" class="form-control form-control-sm" name="notes" id="lf_notes" value="" />
                     </div>
 
-                </div>
 
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">
+                </div>
+                <div
+                    class="flex justify-end items-center gap-x-2 py-3 px-6 border-t border-border dark:border-gray-700">
+                    <button type="button"
+                        class="btn-md text-sm font-medium rounded-md border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                        data-hs-overlay="#large_format_job_modal">
                         Close
                     </button>
-                    <button type="submit" class="btn btn-primary">
-                        <i class="fas fa-check me-3  "></i>
-                        Create Job
+                    <button type="button"
+                        class="btn-md text-sm font-semibold rounded-md border border-transparent bg-primary text-white hover:bg-primaryemphasis disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+                        <i class="fi fi-rr-check me-2"></i>
+                        Save changes
                     </button>
                 </div>
             </form>
