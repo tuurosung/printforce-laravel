3ae855e46ce1b80cff7495672895cb11e4e6792d
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-13 23:41:04 +0000

    Refactor assign job modal to use overlay component and improve layout

--- resources/views/app/job/modals/assign-job-modal.blade.php
+++ resources/views/app/job/modals/assign-job-modal.blade.php
@@ -1,57 +1,45 @@
-<div class="modal fade" id="assignJobModal" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false"
-    role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="modalTitleId">
-                    Assign Job
-                </h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
+<div class="hs-overlay hs-overlay-open:opacity-100 hs-overlay-open:duration-500 hidden size-full fixed top-0 inset-s-0 z-80 opacity-0 overflow-x-hidden transition-all overflow-y-auto pointer-events-none" id="assign-job-modal" hs-overlay="">
+    <div
+        class="hs-overlay-open:opacity-100 hs-overlay-open:duration-500 opacity-0 transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto">
+        <div
+            class="flex flex-col bg-white border shadow-sm rounded-md pointer-events-auto dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]">
+            <x-modals.modal-header modalId="assign-job-modal" modalTitle="Assign Job" />
             <form method="POST" id="assignJobFrm" action="{{ route('jobs.assign-job.assign-to-user') }}">
                 @csrf
-            <div class="modal-body">
+                <div class="p-6">
 
-                <input type="hidden" name="category_id" id="category_id" value="{{ $category_id }}" required readonly/>
+                    <input type="hidden" name="job_type" id="job_type" value="{{ $jobType }}" required readonly/>
 
-                <div class="row">
+                    <div class="grid grid-cols-12 gap-6">
 
-                    <div class="col">
+                        <div class="lg:col-span-6 md:col-span-6 sm:col-span-12 col-span-12">
 
-                        <div class="mb-3">
-                            <label for="job_id" class="form-label">Job ID</label>
-                            <input type="text" class="form-control" name="job_id" id="job_id" value="{{ $job_id }}"
-                                readonly />
+                            <div class="mb-3">
+                                <label for="job_id" class="form-label">Job ID</label>
+                                <input type="text" class="form-control" name="job_id" id="job_id" value="{{ $job->job_id }}"
+                                    readonly />
+                            </div>
+
                         </div>
 
-                    </div>
+                        <div class="lg:col-span-6 md:col-span-6 sm:col-span-12 col-span-12">
 
-                    <div class="col">
+                            <div class="mb-3">
+                                <label for="" class="form-label">Select User</label>
+                                <select class="form-control form-select-lg" name="user_id" id="user_id" required>
+                                    <option value="">--- Select Staff ---</option>
+                                    @foreach ($users as $user)
+                                        <option value="{{ $user->user_id }}">{{ $user->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
 
-                        <div class="mb-3">
-                            <label for="" class="form-label">Select User</label>
-                            <select class="form-select form-select-lg" name="user_id" id="user_id" required>
-                                <option value="">--- Select Staff ---</option>
-                                @foreach ($users as $user)
-                                    <option value="{{ $user->user_id }}">{{ $user->name }}</option>
-                                @endforeach
-                            </select>
                         </div>
 
                     </div>
 
                 </div>
-
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
-                    Close
-                </button>
-                <button type="submit" class="btn btn-primary">
-                    Assign Job
-                    <i class="fi fi-rr-check ms-2"></i>
-                </button>
-            </div>
+            <x-modals.modal-footer btnLabel="Assign Job" modalId="assign-job-modal" />
         </div>
     </div>
 </div>
