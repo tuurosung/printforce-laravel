73caca5c0514e591e6e2df971777437c2129f6ab
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-16 19:48:36 +0000

    Add InvoiceService methods for invoice management
    - Introduced getInvoices method to retrieve all invoices from the repository.
    - Added createInvoice method to handle the creation of new invoices with error handling.
    - Implemented deleteInvoice method to manage the deletion of invoices, including associated items.
    - Updated namespace for InvoiceService to reflect the correct directory structure.

--- app/Domain/Invoices/Services/InvoiceService.php
+++ app/Domain/Invoices/Services/InvoiceService.php
@@ -1,20 +1,59 @@
 <?php
 
-namespace App\Services\Invoices;
+namespace App\Domain\Invoices\Services;
 
 use App\Domain\Invoices\Contracts\InvoiceRepositoryInterface;
 use App\Models\Invoices\CustomerInvoice;
+use DomainException;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 
 class InvoiceService
 {
 
     public function __construct(
-        private readonly InvoiceRepositoryInterface $invoiceRepository
+        private readonly InvoiceRepositoryInterface $invoiceRepository,
+        private readonly CustomerInvoice $model
     ){}
+
+
+    public function getInvoices(): Collection
+    {
+        return $this->invoiceRepository->allInvoices();
+    }
+
+
+    public function createInvoice(array $data)
+    {
+        $newInvoice =  $this->model->create($data);
+
+        if (!$newInvoice) {
+            throw new DomainException("Invoice not created");
+        }
+
+        $this->invoiceRepository->setActiveInvoiceSession($newInvoice);
+        return $newInvoice;
+    }
+
+
+    public function deleteInvoice(CustomerInvoice $customerInvoice)
+    {
+        return DB::transaction(function () use ($customerInvoice) {
+
+            if ($customerInvoice->hasServiceItems()) {
+                $customerInvoice->invoiceItems()->delete();
+            }
+
+            $customerInvoice->delete();
+        });
+    }
 
 
     protected function applyCharges(int $subTotal, CustomerInvoice $invoice): int
     {
         return $subTotal;
     }
+
+
+
 }
