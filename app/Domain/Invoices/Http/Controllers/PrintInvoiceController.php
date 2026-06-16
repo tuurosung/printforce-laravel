4582ca618945d713a8a19c0a4c031ec117ff5a2c
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-16 22:39:15 +0000

    Add PrintInvoiceController for handling invoice printing
    - Introduced PrintInvoiceController to manage the printing of customer invoices.
    - Implemented a method to return the print view with the relevant customer invoice data.
    - Enhanced the organization of invoice-related functionality within the application.

---
+++ app/Domain/Invoices/Http/Controllers/PrintInvoiceController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Domain\Invoices\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Invoices\CustomerInvoice;
+
+class PrintInvoiceController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(CustomerInvoice $customerInvoice)
+    {
+        return view('app.invoices.print-invoice', [
+            'customerInvoice' => $customerInvoice
+        ]);
+    }
+}
