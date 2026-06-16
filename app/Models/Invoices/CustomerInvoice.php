d2ee84d6215d5f504ff3f184968ac202eac9f6c4
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-16 11:27:52 +0000

    Refactor CustomerInvoice model to enhance functionality and structure
    - Introduced InvoiceTypeEnum and InvoiceStatusEnum for better invoice management.
    - Updated invoice ID generation to use a numeric ID generator.
    - Implemented casting for invoice attributes to improve data handling.
    - Renamed customerInvoiceItems method to invoiceItems for clarity.
    - Added draftInvoices scope and totalValue attribute for improved invoice querying and calculations.
    - Removed obsolete methods related to total amount calculation and subtotal updates.

--- app/Models/Invoices/CustomerInvoice.php
+++ app/Models/Invoices/CustomerInvoice.php
@@ -2,20 +2,29 @@
 
 namespace App\Models\Invoices;
 
+use App\Casts\MoneyFormat;
+use App\Enums\Invoices\InvoiceStatusEnum;
+use App\Enums\Invoices\InvoiceTypeEnum;
 use App\Models\Customers\Customer;
-use Illuminate\Support\Facades\Auth;
-use App\Models\Scopes\SubscriberScope;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Builder;
 use App\Models\Invoices\CustomerInvoiceItem;
+use App\Models\Scopes\SubscriberScope;
 use App\Services\Invoices\CustomerInvoiceService;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Scope;
 use Illuminate\Database\Eloquent\Attributes\ScopedBy;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Enum;
 
 #[ScopedBy(SubscriberScope::class)]
+#[Fillable(['subscriber_id', 'customer_id', 'invoice_type', 'invoice_date', 'due_date', 'due_date', 'status', 'sub_total', 'total', 'paid_at'])]
 class CustomerInvoice extends Model
 {
+
+    use HasFactory;
 
     protected static function boot()
     {
@@ -23,29 +32,50 @@ class CustomerInvoice extends Model
 
         static::creating(function ($model) {
             $model->subscriber_id = Auth::user()->subscriber_id;
-            $model->invoice_id = CustomerInvoiceService::generateInvoiceNumber();
+            $model->invoice_id = generatedNumericId(10);
+
         });
     }
 
-    use HasFactory;
     protected $table = 'invoices';
     protected $primaryKey = 'invoice_id';
     protected $keyType = 'string';
     public $incrementing = false;
 
-    protected $fillable = [
-        'subscriber_id',
-        'customer_id',
-        'invoice_type',
-        'invoice_date',
-        'due_date',
-    ];
+
+    protected function casts(): array
+    {
+        return [
+            'invoice_type' => InvoiceTypeEnum::class,
+            'invoice_date' => 'datetime',
+            'due_date' => 'timestamp',
+            'paid_at' => 'datetime',
+            'sub_total' => MoneyFormat::class,
+            'total' => 'decimal:2',
+            'status' => InvoiceStatusEnum::class
+        ];
+    }
 
 
     #[Scope]
     protected function activeInvoices(Builder $query): void
     {
         $query->where('status', 'active');
+    }
+
+
+    #[Scope]
+    protected function draftInvoices(Builder $query): void
+    {
+        $query->where('status', 'draft');
+    }
+
+
+    public function totalValue(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->invoiceItems->sum('total')
+        );
     }
 
 
@@ -63,22 +93,15 @@ class CustomerInvoice extends Model
     }
 
 
-    public function customerInvoiceItems()
+    public function invoiceItems()
     {
         return $this->hasMany(CustomerInvoiceItem::class, 'invoice_id', 'invoice_id');
     }
 
 
-    private function calculateTotalAmount()
+    public function hasItem(string $serviceId): bool
     {
-        return $this->customerInvoiceItems->sum(function ($item) {
-            return $item->total;
-        });
+        return $this->invoiceItems()->where('service_id', $serviceId)->exists();
     }
 
-    public function updateSubTotal()
-    {
-        $this->sub_total = $this->calculateTotalAmount();
-        $this->save();
-    }
 }
