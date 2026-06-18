3eaf4705c5957758ef5cfc2b48a360a005f91906
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-18 19:51:21 +0000

    Refactor LargeFormatJob model for improved structure and functionality
    - Updated the LargeFormatJob model to utilize ScopedBy attribute for subscriber scoping.
    - Removed the ScopedToSubscriber trait to streamline the model.
    - Refactored fillable attributes using the Fillable attribute for better clarity.
    - Enhanced relationship methods to specify return types.
    - Cleaned up unused code and comments for improved readability.

--- app/Models/Jobs/LargeFormatJob.php
+++ app/Models/Jobs/LargeFormatJob.php
@@ -3,29 +3,28 @@
 namespace App\Models\Jobs;
 
 
+use App\Domain\PrintServices\Models\PrintService;
+use App\Enums\Jobs\JobStatusEnum;
+use App\Models\Customers\Customer;
+use App\Models\Scopes\SubscriberScope;
 use App\Models\User;
 use App\Traits\ScopedActive;
-use App\Models\Services\Service;
-use App\Models\Customers\Customer;
-use App\Traits\ScopedToSubscriber;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Auth;
-use App\Models\Services\PrintService;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Scope;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Attributes\ScopedBy;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Auth;
 
+#[ScopedBy([SubscriberScope::class])]
+#[Fillable(['subscriber_id', 'customer_id', 'service_id', 'job_id', 'cost', 'discount', 'premium', 'width', 'height', 'copies', 'total', 'measuring_unit', 'notes', 'date', 'job_assigned_to', 'job_assigned_at', 'job_completed_at', 'job_completed_by'])]
 class LargeFormatJob extends Model
 {
     use HasFactory;
     use ScopedActive;
-    use ScopedToSubscriber;
 
     protected static function boot()
     {
@@ -39,37 +38,17 @@ class LargeFormatJob extends Model
     }
 
 
-
     protected $table = 'jobs_largeformat';
     protected $primaryKey = 'job_id';
     public $incrementing = false;
 
-    protected $fillable = [
-        'subscriber_id',
-        'customer_id',
-        'service_id',
-        'job_id',
-        'cost',
-        'discount',
-        'premium',
-        'width',
-        'height',
-        'copies',
-        'total',
-        'measuring_unit',
-        'notes',
-        'date',
-        'job_assigned_to',
-        'job_assigned_at',
-        'job_completed_at',
-        'job_completed_by',
-    ];
 
-
-    /**
-     * Scopes ------------------------------------------------------------------------------------------
-     */
-
+    protected function casts(): array
+    {
+        return [
+            'job_status'=> JobStatusEnum::class
+        ];
+    }
 
 
     #[Scope]
@@ -85,16 +64,7 @@ class LargeFormatJob extends Model
         $query->where('job_status', 'completed');
     }
 
-    /**
-     * Attributes -----------------------------------------------------------------------------------------
-     */
 
-
-    /**
-     * Get the job details as an array.
-     *
-     * @return Attribute
-     */
     public function details(): Attribute
     {
         return Attribute::make(
@@ -103,11 +73,6 @@ class LargeFormatJob extends Model
     }
 
 
-    /**
-     * Get the job status as a string.
-     *
-     * @return string The job status as a string.
-     */
     public function jobStatusDefinition(): Attribute
     {
         return Attribute::make(
@@ -116,27 +81,12 @@ class LargeFormatJob extends Model
     }
 
 
-
-    /**
-     * Relationships -----------------------------------------------------------------------------------
-     */
-
-
-
-    /**
-     * The customer that owns the large format job.
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
     public function customer():BelongsTo
     {
         return $this->belongsTo(Customer::class, 'customer_id');
     }
 
 
-    /**
-     * The service associated with the large format job.
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
     public function service(): BelongsTo
     {
         return $this->belongsTo(PrintService::class, 'service_id')
@@ -146,33 +96,11 @@ class LargeFormatJob extends Model
     }
 
 
-    /**
-     * The user that the job is assigned to.
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
     public function assignedTo(): BelongsTo
     {
         return $this->belongsTo(User::class, 'job_assigned_to', 'user_id');
     }
 
-
-
-
-
-    // BEGIN STATIC FUNCTIONS
-
-    public static function countLargeFormatJobs()
-    {
-        return LargeFormatJob::count();
-    }
-
-    public static function sumLargeFormatJobsThisYear()
-    {
-        return LargeFormatJob::whereYear('created_at', now()->format('Y'))
-            ->sum('total');
-
-    }
 
     public static function sumLargeFormatJobsThisMonth()
     {
@@ -186,20 +114,4 @@ class LargeFormatJob extends Model
     {
         return "{$this->width} x {$this->height} {$this->measuring_unit} ({$this->copies} pcs)";
     }
-
-
-    private function getJobStatusDescription(): string
-    {
-
-        if ($this->job_status == null || $this->job_status == '') {
-            return 'Unknown';
-        }
-
-        if (!in_array($this->job_status, array_keys(config('printforce.jobs.job_statuses')))) {
-            return 'Unknown';
-        }
-
-        return config('printforce.jobs.job_statuses')[$this->job_status];
-    }
-
 }
