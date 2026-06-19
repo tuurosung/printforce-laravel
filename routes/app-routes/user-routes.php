1ceab84027ddfaec3a74dbb19f0d16dde7ff25df
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-19 22:20:50 +0000

    Refactor user routes for improved organization and clarity
    - Updated route prefixes from 'human-resources' to 'hr' for brevity.
    - Streamlined user password management routes under a dedicated 'users/password' prefix.
    - Replaced individual password reset and update routes with grouped resource routes for better structure.
    - Commented out deprecated route definitions to enhance code readability.

--- routes/app-routes/user-routes.php
+++ routes/app-routes/user-routes.php
@@ -3,25 +3,41 @@
 use App\Http\Controllers\Users\ResetPasswordController;
 use App\Http\Controllers\Users\UpdatePasswordController;
 use App\Http\Controllers\Users\UserController;
+use Illuminate\Support\Facades\Route;
 
-Route::prefix('human-resources')
-    ->name('human-resources.')
+Route::prefix("hr")
+    ->name("hr.")
     ->group(function () {
 
-        // users
-        Route::prefix('users')
-            ->name('users.')
-            ->controller(UserController::class)
+        Route::prefix("users/password")
+            ->name("users.password.")
             ->group(function () {
 
-                Route::get('/', 'index')->name('index');
-                Route::get('/edit/{user}', 'edit')->name('edit');
-                Route::post('/store', 'store')->name('store');
-                Route::post('/update/{user}', 'update')->name('update');
-                Route::delete('/delete/{user}', 'destroy')->name('delete');
-
+                Route::get("/reset/{user}", ResetPasswordController::class)->name("reset");
+                Route::post("/update", UpdatePasswordController::class)->name("update");
             });
 
-        Route::get('/users/reset-password/{user}', ResetPasswordController::class)->name('users.reset-password');
-        Route::post('/users/update-password/{user}', UpdatePasswordController::class)->name('users.update-password');
+        Route::resource("users", UserController::class);
+
     });
+
+// Route::prefix('human-resources')
+//     ->name('human-resources.')
+//     ->group(function () {
+
+//         // users
+//         Route::prefix('users')
+//             ->name('users.')
+//             ->controller(UserController::class)
+//             ->group(function () {
+
+//                 Route::get('/', 'index')->name('index');
+//                 Route::get('/edit/{user}', 'edit')->name('edit');
+//                 Route::post('/store', 'store')->name('store');
+//                 Route::post('/update/{user}', 'update')->name('update');
+//                 Route::delete('/delete/{user}', 'destroy')->name('delete');
+//             });
+
+//         Route::get('/users/reset-password/{user}', ResetPasswordController::class)->name('users.reset-password');
+//         Route::post('/users/update-password/{user}', UpdatePasswordController::class)->name('users.update-password');
+//     });
