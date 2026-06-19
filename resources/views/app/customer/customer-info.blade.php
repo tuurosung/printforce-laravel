bf7f089bbfc0eea3d229cba46efcd06be9cb9089
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-19 17:28:59 +0000

    Update customer-info blade template to enhance search input styling
    - Adjusted SVG markup for search icon to improve rendering.
    - Ensured consistent styling and layout for the search input component.

--- resources/views/app/customer/customer-info.blade.php
+++ resources/views/app/customer/customer-info.blade.php
@@ -176,10 +176,10 @@
                         <div class="relative">
                             <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                                 <!-- <svg class="w-4 h-4 text-body" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
-                                                                                                                        height="24" fill="none" viewBox="0 0 24 24">
-                                                                                                                        <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
-                                                                                                                            d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z" />
-                                                                                                                    </svg> -->
+                                    height="24" fill="none" viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
+                                        d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z" />
+                                </svg> -->
                                 <i class="fi fi-rr-search"></i>
                             </div>
                             <input type="text" id="input-group-1"
