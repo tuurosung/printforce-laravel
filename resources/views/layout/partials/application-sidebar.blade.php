fc1954db636b8e8cde965809219c7ecdf29f7e09
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-19 22:19:00 +0000

    Add application sidebar component for enhanced navigation

---
+++ resources/views/layout/partials/application-sidebar.blade.php
@@ -0,0 +1,155 @@
+<aside id="application-sidebar-brand"
+    class="hs-overlay hs-overlay-open:translate-x-0 -translate-x-full xl:rtl:-translate-x-0 rtl:translate-x-full  left-0 rtl:left-auto rtl:right-0 transform hidden xl:block xl:translate-x-0 xl:end-auto xl:bottom-0 fixed top-0 with-vertical  left-sidebar transition-all duration-300 h-screen xl:z-[2] z-[60] flex-shrink-0 border-r rtl:border-l rtl:border-r-0 w-[270px] border-border dark:border-darkborder bg-white dark:bg-dark">
+    <!-- ---------------------------------- -->
+    <!-- Start Vertical Layout Sidebar -->
+    <!-- ---------------------------------- -->
+    <div class="py-5 px-5 flex justify-between">
+        <div class="brand-logo flex  items-center w-full">
+            <a href="../main/index.html" class="text-nowrap logo-img">
+                <img src="{{ asset('images/logo.png') }}" class="dark:hidden block rtl:hidden w-40!" alt="Logo-Dark">
+                <img src="{{ asset('images/logo.png') }}" class="dark:block hidden rtl:hidden rtl:dark:hidden w-fit!" alt="Logo-light">
+
+                <img src="{{ asset('images/logo.png') }}"
+                    class="dark:hidden hidden rtl:block rtl:dark:hidden w-fit!" alt="Logo-Dark">
+                <img src="{{ asset('images/logo.png') }}"
+                    class="dark:hidden hidden rtl:hidden rtl:dark:block w-fit!" alt="Logo-light">
+            </a>
+        </div>
+
+    </div>
+    <div class="overflow-hidden">
+        <div class="scroll-sidebar simplebar-scrollable-y" data-simplebar="init">
+            <div class="simplebar-wrapper" style="margin: 0px;">
+                <div class="simplebar-height-auto-observer-wrapper">
+                    <div class="simplebar-height-auto-observer"></div>
+                </div>
+                <div class="simplebar-mask">
+                    <div class="simplebar-offset" style="right: 0px; bottom: 0px;">
+                        <div class="simplebar-content-wrapper" tabindex="0" role="region"
+                            aria-label="scrollable content" style="height: 100%; overflow: hidden scroll;">
+                            <div class="simplebar-content" style="padding: 0px;">
+                                <div class="px-6 mt-8 mini-layout" data-te-sidenav-menu-ref="">
+                                    <nav class="hs-accordion-group w-full flex flex-col">
+                                        <ul data-te-sidenav-menu-ref="" id="sidebarnav">
+
+
+
+                                            <li class="sidebar-item">
+                                                <a class="sidebar-link dark-sidebar-link flex align-middle" href="{{ route('dashboard') }}">
+                                                    <i class="fi fi-rr-paper-plane shrink-0"></i>
+                                                    <span class="hide-menu shrink-0 my-0">Dashboard</span>
+                                                </a>
+                                            </li>
+                                            <x-sidebar.sidebar-item label="Customers" icon="users" href="{{ route('customers.customer.index') }}" />
+                                            <x-sidebar.sidebar-item label="Jobs" icon="briefcase" href="{{ route('jobs.today') }}" class="" />
+                                            <x-sidebar.sidebar-item label="Debtors" icon="sack-dollar" href="{{ route('customers.debtors.view') }}" />
+
+                                            <x-sidebar.sidebar-item label="Payments" icon="payroll-check" href="{{ route('payments.index') }}" />
+                                            <x-sidebar.sidebar-item label="Invoices" icon="file-invoice-dollar" href="{{ route('invoices.index') }}" />
+
+
+                                            <x-sidebar.sidebar-item label="Suppliers" icon="seller-store" href="{{ route('suppliers.index') }}" class="mt-8" />
+                                            <x-sidebar.sidebar-item label="Purchases" icon="shopping-cart" href="{{ route('purchases.index') }}" />
+
+                                            <li class="sidebar-item">
+                                                <a class="sidebar-link dark-sidebar-link flex align-middle" href="{{ route('dashboard') }}">
+                                                    <i class="fi fi-rr-money-bill-transfer shrink-0"></i>
+                                                    <span class="hide-menu shrink-0 my-0">Purchase Payments</span>
+                                                </a>
+                                            </li>
+
+                                            <x-sidebar.sidebar-item label="Expenses" icon="chart-histogram" href="{{ route('accounting.expenditure.index') }}" class="mt-5" />
+
+
+                                            <li class="sidebar-item">
+                                                <a class="sidebar-link dark-sidebar-link flex align-middle" href="{{ route('dashboard') }}">
+                                                    <i class="fi fi-rr-member-list shrink-0"></i>
+                                                    <span class="hide-menu shrink-0 my-0">Chart Of Accounts</span>
+                                                </a>
+                                            </li>
+                                            <li class="sidebar-item mt-5">
+                                                <a class="sidebar-link dark-sidebar-link flex align-middle" href="{{ route('dashboard') }}">
+                                                    <i class="fi fi-rr-file-invoice-dollar shrink-0"></i>
+                                                    <span class="hide-menu shrink-0 my-0">Fund Transer</span>
+                                                </a>
+                                            </li>
+                                            <li class="sidebar-item">
+                                                <a class="sidebar-link dark-sidebar-link flex align-middle" href="{{ route('dashboard') }}">
+                                                    <i class="fi fi-rr-file-invoice-dollar shrink-0"></i>
+                                                    <span class="hide-menu shrink-0 my-0">Employees</span>
+                                                </a>
+                                            </li>
+                                            <li class="sidebar-item">
+                                                <a class="sidebar-link dark-sidebar-link flex align-middle" href="{{ route('dashboard') }}">
+                                                    <i class="fi fi-rr-file-invoice-dollar shrink-0"></i>
+                                                    <span class="hide-menu shrink-0 my-0">Registered Users</span>
+                                                </a>
+                                            </li>
+                                            <x-sidebar.sidebar-item label="Registered Users" icon="users" href="{{ route('hr.users.index') }}" />
+                                            <x-sidebar.sidebar-item label="Services" icon="settings" href="{{ route('print-services.index') }}" />
+
+                                            <li class="sidebar-item mt-6">
+                                                <a class=" sidebar-link dark-sidebar-link " href="javascript:void(0)">
+                                                    <i class="ti ti-award flex-shrink-0 text-xl"></i> <span
+                                                        class="hide-menu">Chip</span>
+                                                    <span
+                                                        class="ms-auto bg-primary text-white h-6 w-6 rounded-full text-xs flex  items-center justify-center hide-menu-flex">9</span>
+
+                                                </a>
+                                            </li>
+                                            <li class="sidebar-item">
+                                                <a class=" sidebar-link dark-sidebar-link
+           " href="javascript:void(0)">
+                                                    <i class="ti ti-mood-smile flex-shrink-0  text-xl"></i> <span
+                                                        class="hide-menu">Outlined</span>
+                                                    <span
+                                                        class="ms-auto text-primary outline outline-1 rounded-full outline-primary text-center px-2 text-xs py-1 hide-menu">outline</span>
+                                                </a>
+                                            </li>
+
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="simplebar-placeholder" style="width: 269px; height: 3705px;"></div>
+            </div>
+            <div class="simplebar-track simplebar-horizontal" style="visibility: hidden;">
+                <div class="simplebar-scrollbar" style="width: 0px; display: none;"></div>
+            </div>
+            <div class="simplebar-track simplebar-vertical" style="visibility: visible;">
+                <div class="simplebar-scrollbar"
+                    style="height: 25px; transform: translate3d(0px, 0px, 0px); display: block;"></div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bottom User Profile -->
+    <div class="px-6 pt-6  bg-surface  dark:bg-dark-surface relative">
+        <div class="bg-lightsecondary dark:bg-darksecondary p-4 rounded-md">
+            <div class="hide-menu ">
+                <div class="flex items-center">
+                    <img src="{{ asset('images/user-1.jpg') }}" class="h-9 w-9 rounded-full object-cover"
+                        alt="profile">
+                    <div class="ml-4 rtl:mr-4 rtl:ml-0">
+                        <h5 class="text-base font-semibold text-dark dark:text-white">{{ auth()->user()->name }}</h5>
+                        <p class="text-xs font-normal text-link dark:text-darklink ">Designer</p>
+                    </div>
+                    <div class="ms-auto hs-tooltip hs-tooltip-toggle">
+                        <a href="javascript:void(0)"><i class="ti ti-power text-primary me-3 text-2xl "></i>
+                            <span
+                                class="tooltip hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible"
+                                role="tooltip" style="position: fixed; left: 182.208px; top: 180.85px;">
+                                Logout
+                            </span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- </aside> -->
+
+</aside>
