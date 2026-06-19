f5a38233d30692e5f239e59269840eada783f1ad
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-19 22:20:33 +0000

    Refactor User model to utilize Fillable attribute and AccessLevelEnum
    - Replaced the traditional $fillable array with the #[Fillable] attribute for improved clarity and type safety.
    - Integrated AccessLevelEnum for casting the access level attribute, enhancing maintainability and consistency in user role management.
    - Removed deprecated comments and streamlined the code structure for better readability.

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,12 +4,15 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
+use App\Enums\Users\AccessLevelEnum;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+#[Fillable(['subscriber_id', 'name', 'email','password', 'access_level', 'first_name', 'last_name'])]
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
@@ -26,62 +29,24 @@ class User extends Authenticatable
 
     protected $primaryKey = 'user_id';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'subscriber_id',
-        'name',
-        'email',
-        'phone_number',
-        'password',
-        'access_level',
-        'firstname',
-        'lastname'
-    ];
 
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'access_level' => AccessLevelEnum::class
         ];
     }
 
-    /**
-     * Attributes ------------------------------------------------------------------------
-     */
 
 
-     public function accessLevelName(): Attribute
-     {
-        return Attribute::make(
-            get: fn () => $this->getAccessLevelName()
-        );
-     }
-
-
-    /**
-     * Relationships ---------------------------------------------------------------------
-     */
 
     public function company()
     {
