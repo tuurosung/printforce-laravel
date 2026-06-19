6dd0901dcf48f462d03d0b734483d1bebca64478
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-19 22:17:01 +0000

    Refactor new user modal for improved structure and usability
    - Replaced static modal structure with dynamic Blade components for better organization.
    - Enhanced form layout using a grid system for improved responsiveness.
    - Updated action routes to align with RESTful practices and adjusted input fields for consistency.
    - Integrated Recaptcha for enhanced security during user creation.

--- resources/views/app/users/modals/new-user-modal.blade.php
+++ resources/views/app/users/modals/new-user-modal.blade.php
@@ -1,161 +1,109 @@
-<div
-    class="modal fade"
-    id="new_user_modal"
-    tabindex="-1"
+@use(Lunaweb\RecaptchaV3\Facades\RecaptchaV3)
 
-    role="dialog"
-    aria-labelledby="modalTitleId"
-    aria-hidden="true">
-    <div
-        class="modal-dialog"
-        role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="modalTitleId">
-                    Add New User
-                </h5>
-                <button
-                    type="button"
-                    class="btn-close"
-                    data-dismiss="modal"
-                    data-bs-dismiss="modal"
-                    aria-label="Close"></button>
-            </div>
-            <form method="POST" action="{{ route('human-resources.users.store') }}" autocomplete="off">
-                @csrf
-                <div class="modal-body">
+<x-modals.modal modalId="new-user-modal">
+    <x-modals.modal-header modalId="new-user-modal" modalTitle="Create New User" />
 
-                    <div class="row">
-                        <div class="col">
-                            <div class="mb-3">
-                                <label for="firstname" class="form-label">First Name</label>
-                                <input
-                                    type="text"
-                                    class="form-control"
-                                    name="firstname"
-                                    id="firstname"
-                                    placeholder="your first name"
-                                    required
-                                />
-                            </div>
+    <form method="POST" action="{{ route('hr.users.store') }}" autocomplete="off">
+        @csrf
 
-                        </div>
-                        <div class="col">
-                            <div class="mb-3">
-                                <label for="lastname" class="form-label">Last Name</label>
-                                <input
-                                    type="text"
-                                    class="form-control"
-                                    name="lastname"
-                                    id="lastname"
-                                    aria-describedby="helpId"
-                                    placeholder="your last name"
-                                    required
-                                />
-                            </div>
+        <div class="p-6">
 
-                        </div>
-                    </div>
+            <div class="grid grid-cols-12 gap-6">
 
-                    <div class="row">
-                        <div class="col">
-
-                            <div class="mb-3">
-                                <label for="" class="form-label">Phone Number</label>
-                                <input
-                                    type="text"
-                                    class="form-control form-control-sm"
-                                    name="phone_number"
-                                    id="phone_number"
-                                    value=""
-                                    required>
-                            </div>
-
-                        </div>
-                        <div class="col">
-
-                            <div class="mb-3">
-                                <label for="" class="form-label">Access Level</label>
-                                <select
-                                    class="form-select form-select-sm"
-                                    name="access_level"
-                                    id="access_level"
-                                    value="">
-
-                                    @foreach (config('printforce.users.access_levels') as $key => $value)
-
-                                    <option value="{{ $key }}">{{ $value }}</option>
-
-                                    @endforeach
-                                </select>
-                            </div>
-
-                        </div>
-                    </div>
-
+                <div class="lg:col-span-6 md:span-6 sm:col-span-12 col-span-12">
                     <div class="mb-3">
-                        <label for="" class="form-label">Email</label>
+                        <label for="firstname" class="form-label">First Name</label>
                         <input
-                            type="email"
-                            class="form-control form-control-sm"
-                            name="email"
-                            id="email"
+                            type="text"
+                            class="form-control"
+                            name="firstname"
+                            id="firstname"
+                            placeholder=""
+                            required />
+                    </div>
+                </div>
+
+                <div class="lg:col-span-6 md:span-6 sm:col-span-12 col-span-12">
+                    <div class="mb-3">
+                        <label for="lastname" class="form-label">Last Name</label>
+                        <input
+                            type="text"
+                            class="form-control"
+                            name="lastname"
+                            id="lastname"
+                            aria-describedby="helpId"
+                            placeholder=""
+                            required />
+                    </div>
+                </div>
+                <div class="lg:col-span-6 md:span-6 sm:col-span-12 col-span-12">
+                    <label for="" class="form-label">Phone Number</label>
+                    <input
+                        type="text"
+                        class="form-control"
+                        name="phone_number"
+                        id="phone_number"
+                        value=""
+                        required>
+                </div>
+                <div class="lg:col-span-6 md:span-6 sm:col-span-12 col-span-12">
+                    <label for="" class="form-label">Access Level</label>
+                    <select
+                        class="form-control"
+                        name="access_level"
+                        id="access_level"
+                        value="">
+
+                        @foreach (\App\Enums\Users\AccessLevelEnum::options() as $key => $value)
+
+                        <option value="{{ $key }}">{{ $value }}</option>
+
+                        @endforeach
+                    </select>
+                </div>
+                <div class="lg:col-span-12 md:span-12 sm:col-span-12 col-span-12">
+                    <label for="" class="form-label">Email</label>
+                    <input
+                        type="email"
+                        class="form-control"
+                        name="email"
+                        id="email"
+                        value=""
+                        required />
+                </div>
+                <div class="lg:col-span-6 md:span-6 sm:col-span-12 col-span-12">
+                    <div class="mb-3">
+                        <label for="" class="form-label">Password</label>
+                        <input
+                            type="password"
+                            class="form-control"
+                            name="password"
+                            id="password"
                             value=""
                             required />
                     </div>
-
-                    <div class="row">
-                        <div class="col">
-                            <div class="mb-3">
-                                <label for="" class="form-label">Password</label>
-                                <input
-                                    type="password"
-                                    class="form-control form-control-sm"
-                                    name="password"
-                                    id="password"
-                                    value=""
-                                    required />
-                            </div>
-                        </div>
-
-                        <div class="col">
-
-                            <div class="mb-3">
-                                <label for="confirm_password" class="form-label">Confirm Password</label>
-                                <input
-                                    type="password"
-                                    class="form-control"
-                                    name="confirm_password"
-                                    id="confirm_password"
-                                    required />
-                            </div>
-
-                        </div>
-
-
-
+                </div>
+                <div class="lg:col-span-6 md:span-6 sm:col-span-12 col-span-12">
+                    <div class="mb-3">
+                        <label for="confirm_password" class="form-label">Confirm Password</label>
+                        <input
+                            type="password"
+                            class="form-control"
+                            name="confirm_password"
+                            id="confirm_password"
+                            required />
                     </div>
-
-                    @use(Lunaweb\RecaptchaV3\Facades\RecaptchaV3)
-
-
-                    {!! RecaptchaV3::field('create') !!}
-
                 </div>
-                <div class="modal-footer">
-                    <button
-                        type="button"
-                        class="btn btn-secondary"
-                        data-dismiss="modal"
-                        data-bs-dismiss="modal">
-                        Close
-                    </button>
-                    <button type="submit" class="btn btn-primary" value="create">
-                        <i class="fas fa-check me-3  "></i>
-                        Create User
-                    </button>
-                </div>
-            </form>
+            </div>
+
         </div>
-    </div>
-</div>
+
+
+        {!! \RecaptchaV3::field('create') !!}
+
+
+        <x-modals.modal-footer modalId="new-user-modal" btnLabel="Create User" />
+
+    </form>
+</x-modals.modal>
+
