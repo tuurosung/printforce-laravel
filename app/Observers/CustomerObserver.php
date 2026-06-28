3f8088912f53db44644f2260f200bbd4cf4214ac
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-28 01:56:00 +0000

    Add CustomerObserver to manage customer lifecycle events
    - Introduced CustomerObserver class to handle creating, created, updated, deleted, restored, and force deleted events for customers.
    - The creating method sets subscriber_id and generates a unique customer_id, ensuring consistent customer management.
    - Placeholder methods for other events are included for future implementation.

---
+++ app/Observers/CustomerObserver.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Customers\Customer;
+
+class CustomerObserver
+{
+
+    public function creating(Customer $customer): void
+    {
+        $customer->subscriber_id = auth()->user()->subscriber_id;
+        $customer->customer_id = generateRandomString();
+    }
+
+    /**
+     * Handle the Customer "created" event.
+     */
+    public function created(Customer $customer): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Customer "updated" event.
+     */
+    public function updated(Customer $customer): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Customer "deleted" event.
+     */
+    public function deleted(Customer $customer): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Customer "restored" event.
+     */
+    public function restored(Customer $customer): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Customer "force deleted" event.
+     */
+    public function forceDeleted(Customer $customer): void
+    {
+        //
+    }
+}
