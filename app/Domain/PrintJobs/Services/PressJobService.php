fbf57036d9aa00f0da94131bbfb65e42fff9b69d
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-28 11:24:12 +0000

    Add PressJobService for managing press job operations
    - Introduced PressJobService to handle the creation, updating, and deletion of press jobs.
    - Implemented error handling and logging for each operation to enhance reliability and traceability.
    - This service adheres to the PressJobServiceInterface, ensuring consistency in press job management within the application.

---
+++ app/Domain/PrintJobs/Services/PressJobService.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace App\Domain\PrintJobs\Services;
+
+use App\Domain\PrintJobs\Contracts\PressJobServiceInterface;
+use App\Domain\PrintJobs\Models\PressJob;
+use App\Models\Customers\Customer;
+use Illuminate\Support\Facades\Log;
+
+class PressJobService implements PressJobServiceInterface
+{
+
+    public function __construct(
+        private PressJob $model
+    ){}
+
+
+    public function create(Customer $customer, array $data): PressJob
+    {
+        $pressJob = $customer->pressJobs()->create($data);
+
+        if (! $pressJob) {
+            $error = "Unable to create new press job";
+            Log::error($error);
+            throw new \Exception($error);
+        }
+
+        return $pressJob;
+    }
+
+
+    public function update(PressJob $pressJob, array $data): bool
+    {
+        $isUpdated = $pressJob->update($data);
+
+        if (! $isUpdated) {
+            $error = "Unable to update Press Job";
+            Log::error($error);
+            throw new \Exception($error);
+        }
+
+        return true;
+    }
+
+    public function delete(PressJob $pressJob): bool
+    {
+        $isDeleted = $pressJob->delete();
+
+        if (! $isDeleted) {
+            $error = "Unable to delete Press Job";
+            Log::error($error);
+            throw new \Exception($error);
+        }
+
+        return true;
+    }
+
+
+
+    // public function getLatestPressJobs()
+    // {
+    //     return $this->pressJob->with('customer', 'service')
+    //         ->latest('date')->take(100)->get();
+    // }
+
+
+    // public function getTodaysPressJobs()
+    // {
+    //     return $this->pressJob->with('customer', 'service')
+    //         ->whereDate('created_at', now()->format('Y-m-d'))
+    //         ->latest('date')->get();
+    // }
+
+
+    // public function getPressJobsByDateRange(string $start_date, string $end_date)
+    // {
+    //     $start_date = Carbon::parse($start_date)->startOfDay();
+    //     $end_date = Carbon::parse($end_date)->endOfDay();
+
+    //     return $this->pressJob->with('customer', 'service')
+    //         ->whereBetween('created_at', [$start_date, $end_date])
+    //         ->latest('date')->get();
+    // }
+
+
+    // public function getPressJobStatistics()
+    // {
+    //     $carbonNow = Carbon::now();
+
+    //     $statistics = $this->pressJob->query()
+    //         ->selectRaw('
+    //             COUNT(CASE WHEN DATE(date) = CURDATE() THEN 1 ELSE NULL END) as todays_job_count,
+    //             SUM(CASE WHEN DATE(date) = CURDATE() THEN total ELSE 0 END) as todays_jobs_total,
+
+    //             COUNT(CASE WHEN MONTH(date) = MONTH(CURDATE()) AND YEAR(date) = YEAR(CURDATE()) THEN 1 ELSE NULL END) as this_months_job_count,
+    //             SUM(CASE WHEN MONTH(date) = MONTH(CURDATE()) AND YEAR(date) = YEAR(CURDATE()) THEN total ELSE 0 END) as this_months_jobs_total,
+
+    //             COUNT(CASE WHEN YEAR(date) = YEAR(CURDATE()) THEN 1 ELSE NULL END) as this_years_job_count,
+    //             SUM(CASE WHEN YEAR(date) = YEAR(CURDATE()) THEN total ELSE 0 END) as this_years_jobs_total,
+
+    //             COUNT(*) as all_time_job_count,
+    //             SUM(total) as all_time_jobs_total
+    //         ')->first();
+
+    //     return [
+    //         'todays_job_count' => $statistics->todays_job_count ?? 0,
+    //         'todays_jobs_total' => $statistics->todays_jobs_total ?? 0,
+
+    //         'this_months_job_count' => $statistics->this_months_job_count ?? 0,
+    //         'this_months_jobs_total' => $statistics->this_months_jobs_total ?? 0,
+
+    //         'this_years_job_count' => $statistics->this_years_job_count ?? 0,
+    //         'this_years_jobs_total' => $statistics->this_years_jobs_total ?? 0,
+
+    //         'all_time_job_count' => $statistics->all_time_job_count ?? 0,
+    //         'all_time_jobs_total' => $statistics->all_time_jobs_total ?? 0
+    //     ];
+    // }
+
+
+    // public function getTodaysJobsCount()
+    // {
+    //     $statistics = $this->getPressJobStatistics();
+    //     return $statistics['todays_job_count'] ?? 0;
+    // }
+
+
+    // public function getTodaysJobsTotal()
+    // {
+    //     $statistics = $this->getPressJobStatistics();
+    //     return $statistics['todays_jobs_total'] ?? 0;
+    // }
+
+
+    // public function getMonthlyJobCount()
+    // {
+    //     $statistics = $this->getPressJobStatistics();
+    //     return $statistics['this_months_job_count'] ?? 0;
+    // }
+
+
+    // public function getMonthlyJobTotal()
+    // {
+    //     $statistics = $this->getPressJobStatistics();
+    //     return $statistics['this_months_jobs_total'] ?? 0;
+    // }
+
+
+    // public function getYearlyJobCount()
+    // {
+    //     $statistics = $this->getPressJobStatistics();
+    //     return $statistics['this_years_job_count'] ?? 0;
+    // }
+
+    // public function getYearlyJobTotal()
+    // {
+    //     $statistics = $this->getPressJobStatistics();
+    //     return $statistics['this_years_jobs_total'] ?? 0;
+    // }
+
+    // public function getAllTimeJobCount()
+    // {
+    //     $statistics = $this->getPressJobStatistics();
+    //     return $statistics['all_time_job_count'] ?? 0;
+    // }
+
+    // public function getAllTimeJobTotal()
+    // {
+    //     $statistics = $this->getPressJobStatistics();
+    //     return $statistics['all_time_jobs_total'] ?? 0;
+    // }
+}
