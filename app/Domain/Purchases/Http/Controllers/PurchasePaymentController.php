b27594ae16c3b6a229bf08aff82c32f746acf5b0
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-29 08:47:29 +0000

    Add PurchasePaymentController for managing purchase payments
    - Introduced PurchasePaymentController to handle CRUD operations for purchase payments.
    - Implemented methods for listing, creating, editing, updating, and deleting purchase payments.
    - Integrated PurchasePaymentService for business logic and AccountService for account-related data.
    - This addition enhances the management of purchase payments within the application, improving user experience and data handling.

---
+++ app/Domain/Purchases/Http/Controllers/PurchasePaymentController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Domain\Purchases\Http\Controllers;
+
+use App\Domain\Purchases\Services\PurchasePaymentService;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Purchases\StorePurchasePaymentRequest;
+use App\Models\Purchases\PurchasePayment;
+use App\Services\Accounting\AccountService;
+use App\Traits\HandleResourceActions;
+
+class PurchasePaymentController extends Controller
+{
+
+    use HandleResourceActions;
+
+
+
+    /**
+     * Create a new controller instance.
+     */
+    public function __construct(
+        private AccountService $accountService,
+        private PurchasePaymentService $purchasePaymentService
+    ){}
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $payments = $this->purchasePaymentService->getRecentPayments();
+        return view('app.purchase-payments.purchase-payments', [
+            'payments' => $payments
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePurchasePaymentRequest $request)
+    {
+        $this->purchasePaymentService->createPurchase($request->validated());
+        return redirect()->back()->with('success', "Purchase payment recorded successfully");
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PurchasePayment $purchasePayment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PurchasePayment $purchasePayment)
+    {
+        return view('app.purchase-payments.modals.edit-purchase-payment', [
+            'purchasePayment' => $purchasePayment,
+            'supplier' => $purchasePayment->supplier,
+            'operating_accounts' => $this->accountService->getAssetAccounts()
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(StorePurchasePaymentRequest $request, PurchasePayment $purchasePayment)
+    {
+        return $this->handleUpdate($request, $purchasePayment);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PurchasePayment $purchasePayment)
+    {
+        return $this->handleDelete($purchasePayment);
+    }
+
+}
