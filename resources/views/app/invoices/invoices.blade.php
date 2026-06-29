a53bafa734807a2e32f97fd004af3e8494f5622f
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-29 20:44:23 +0000

    Enhance Invoices Dropdown Item with Print Class
    - Added a specific class "print-invoice" to the Print dropdown item for improved styling and functionality.
    - This change enhances the maintainability and clarity of the invoices view, aligning with the overall structure of the application.

--- resources/views/app/invoices/invoices.blade.php
+++ resources/views/app/invoices/invoices.blade.php
@@ -86,7 +86,7 @@
                                         <div class="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white custom-shadow rounded-md p-2 mt-2 dark:bg-gray-800 dark:border dark:border-gray-700 dark:divide-gray-700 after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full z-10"
                                             aria-labelledby="hs-dropdown-default">
 
-                                                <x-dropdowns.dropdown-item icon="print" iconColour="primary" label="Print" data-url="{{ route('invoices.print-invoice', $customerInvoice) }}" />
+                                                <x-dropdowns.dropdown-item class="print-invoice" icon="print" iconColour="primary" label="Print" data-url="{{ route('invoices.print-invoice', $customerInvoice) }}" />
                                                 <x-dropdowns.dropdown-item icon="edit" iconColour="primary" label="Edit" href="{{ route('invoices.prepare-customer-invoice', $customerInvoice) }}" />
 
                                                 <form action="{{ route('invoices.delete', $customerInvoice) }}" method="POST">
