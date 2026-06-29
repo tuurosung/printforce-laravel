9b21801d0c8726d5056d09aeee353e8e2c4b676f
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-29 19:53:32 +0000

    Refactor Edit Purchase Payment Modal for Improved UI and Functionality
    - Updated the modal structure to use a new component-based approach for better maintainability.
    - Enhanced the form layout with a grid system for improved responsiveness and user experience.
    - Added a supplier selection dropdown and a payment date input for more comprehensive payment details.
    - Streamlined the form submission process with proper CSRF and method handling.
    - This refactor improves the overall usability and clarity of the edit purchase payment functionality.

--- resources/views/app/purchase-payments/modals/edit-purchase-payment.blade.php
+++ resources/views/app/purchase-payments/modals/edit-purchase-payment.blade.php
@@ -1,31 +1,58 @@
-<x-printforce.modals.form-modal modalId="edit_payment_modal" modalTitle="Edit Payment" modalSize="modal-lg"
-    formType="edit" btnLabel="Update Payment" :action="route('purchases.payments.update', $purchasePayment)">
+<x-modals.modal modalId="edit_payment_modal">
+    <x-modals.modal-header modalId="edit_payment_modal" modalTitle="Edit Purchase Payment" />
 
-    <div class="row">
-        <div class="col-6">
-            <x-printforce.input.number-input name="amount_paid" id="amount_paid" label="Amount Paid"
-                value="{{ $purchasePayment->amount_paid }}" placeholder="Enter amount paid" required="true" />
+    <form method="POST" action="{{ route('purchase-payment.update', $purchasePayment) }}">
+        @csrf
+        @method('PATCH')
+        <div class="p-6">
+
+            <div class="grid grid-cols-12 gap-6">
+
+                <div class="lg:col-span-6 md:col-span-6 sm:col-span-12 col-span-12">
+                    <label for="" class="form-label">Supplier</label>
+                    <select name="supplier_id" id="supplier_id" class="form-control">
+                        <option value="{{ $purchasePayment->supplier_id }}">{{ $purchasePayment->supplier->supplier_name }}</option>
+                    </select>
+                </div>
+
+                <div class="lg:col-span-6 md:col-span-6 sm:col-span-12 col-span-12"></div>
+
+                <div class="lg:col-span-6 md:col-span-6 sm:col-span-12 col-span-12">
+                    <x-printforce.input.number-input name="amount_paid" id="amount_paid" label="Amount Paid"
+                        value="{{ $purchasePayment->amount_paid }}" placeholder="Enter amount paid" required="true" />
+                </div>
+
+                <div class="lg:col-span-6 md:col-span-6 sm:col-span-12 col-span-12">
+                    <x-printforce.inputs.text-input name="reference" id="reference" label="Cheque # / Reference"
+                        placeholder="Enter cheque number or reference" value="{{ $purchasePayment->reference }}" required="true" />
+                </div>
+
+                <div class="lg:col-span-6 md:col-span-6 sm:col-span-12 col-span-12">
+                    <x-printforce.inputs.date-input name="date" id="date" label="Payment Date" value="{{ now()->format('Y-m-d') }}"
+                        required="true" value="{{ $purchasePayment->date }}" />
+                </div>
+
+                <div class="lg:col-span-6 md:col-span-6 sm:col-span-12 col-span-12">
+                    <x-printforce.inputs.select-input name="account_number" id="account_number" label="Payment Account"
+                        :options="$operating_accounts" :selected="$purchasePayment->account_number" required="true" />
+                </div>
+
+                <div class="lg:col-span-12 md:col-span-12 sm:col-span-12 col-span-12">
+                    <x-printforce.inputs.text-input name="notes" id="notes" label="Notes"
+                        value="{{ $purchasePayment->notes ?? 'Payment for material supply' }}"
+                        placeholder="Enter any additional notes here" />
+                </div>
+
+
+
+
+            </div>
+
         </div>
-        <div class="col-6">
-            <x-printforce.inputs.text-input name="reference" id="reference" label="Cheque # / Reference"
-                placeholder="Enter cheque number or reference" value="{{ $purchasePayment->reference }}" required="true" />
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-6">
-            <x-printforce.inputs.date-input name="date" id="date" label="Payment Date" value="{{ now()->format('Y-m-d') }}"
-                required="true" value="{{ $purchasePayment->date }}" />
-        </div>
-        <div class="col-6">
-            <x-printforce.inputs.select-input name="account_number" id="account_number" label="Payment Account"
-                :options="$operating_accounts" :selected="$purchasePayment->account_number" required="true" />
-        </div>
-    </div>
-
-    <x-printforce.inputs.text-input name="notes" id="notes" label="Notes"
-        value="{{ $purchasePayment->notes ?? 'Payment for material supply' }}"
-        placeholder="Enter any additional notes here" />
 
 
-</x-printforce.modals.form-modal>
+
+        <x-modals.modal-footer modalId="edit_payment_modal" btnLabel="Update Payment Info" />
+
+    </form>
+</x-modals.modal>
