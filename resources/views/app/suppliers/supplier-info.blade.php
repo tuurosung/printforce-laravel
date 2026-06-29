94fbecac5436f690229aa970e22f61a43bf4ef82
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-29 20:44:49 +0000

    Refactor Supplier Info Tabs for Enhanced Structure and Usability
    - Replaced the existing tab structure with a new Blade component for improved organization and maintainability.
    - Streamlined the tab content management, ensuring better accessibility and clarity in the user interface.
    - Included necessary modals for creating new purchases and payments, enhancing the overall functionality of the supplier info page.

--- resources/views/app/suppliers/supplier-info.blade.php
+++ resources/views/app/suppliers/supplier-info.blade.php
@@ -38,43 +38,43 @@
     </div>
 </div>
 
-<div class="card mb-5">
-    <div class="card-body">
+<x-tabs.tab>
+    <x-slot name="tabs">
+        <x-tabs.tab-item id="dashboard-tab" label="Dashboard" :active="true" controls="dashboard-content" />
+        <x-tabs.tab-item id="invoices-tab" label="Invoices" :active="false" controls="invoices-content" />
+        <x-tabs.tab-item id="payments-tab" label="Payments" :active="false" controls="payments-content" />
+    </x-slot>
+    <x-slot name="content">
 
-        <div class="flex">
-            <div
-                class="flex bg-gray-100 hover:bg-gray-200 rounded-md transition p-1 dark:bg-gray-700 dark:hover:bg-gray-600">
-                <nav class="flex space-x-2 " aria-label="Tabs" role="tablist">
+        <x-tabs.tab-content
+            id="dashboard-content"
+            labelledby="dashboard-tab"
+            :active="true">
 
-                    <x-tabs.tab-item id="dashboard-tab" label="Dashboard" icon="paper-plane"
-                        dataHsTab="dashboard-content" :ariaSelected="true" />
-                    <x-tabs.tab-item id="invoices-tab" label="Invoices" icon="file-invoice-dollar"
-                        dataHsTab="invoices-content" :ariaSelected="false" />
-                    <x-tabs.tab-item label="Payments" icon="sack-dollar" dataHsTab="payments-content"
-                        :ariaSelected="false" />
-
-                </nav>
-            </div>
-        </div>
-
-        <div class="mt-3">
-            <div id="dashboard-content" role="tabpanel" aria-labelledby="dashboard-tab" class="pt-5">
-                @include('app.suppliers.partials.dashboard')
-            </div>
-            <div id="invoices-content" role="tabpanel" aria-labelledby="invoices-tab" class="pt-5 hidden">
-                @include('app.suppliers.partials.purchase-invoices')
-            </div>
-            <div id="payments-content" role="tabpanel" aria-labelledby="payments-tab" class="pt-5 hidden">
-                @include('app.suppliers.partials.purchase-payments')
-            </div>
-        </div>
-
-    </div>
-</div>
+        </x-tabs.tab-content>
 
 
+        <x-tabs.tab-content
+            id="invoices-content"
+            labelledby="invoices-tab"
+            :active="false">
+
+            @include('app.suppliers.partials.purchase-invoices')
+
+        </x-tabs.tab-content>
 
 
+        <x-tabs.tab-content
+            id="payments-content"
+            labelledby="payments-tab"
+            :active="false">
+
+            @include('app.suppliers.partials.purchase-payments')
+
+        </x-tabs.tab-content>
+
+    </x-slot>
+</x-tabs.tab>
 
 @include('app.suppliers.modals.create-new-purchase')
 @include('app.suppliers.modals.new-purchase-payment')
@@ -84,69 +84,27 @@
 @section('js')
 
 <script>
-$(document).ready(function() {
+    $(document).ready(function() {
 
-    $('#date_issued, #due_date').datepicker();
+        $('#date_issued, #due_date').datepicker();
 
-    $('#date_issued, #due_date').on('changeDate', function() {
-        $(this).datepicker('hide');
-    })
-
-    $('#new_payment_modal').on('shown.bs.modal', function() {
-        setTimeout(function() {
-            $('#amount_paid').focus();
-        }, 300)
-    })
-
-    $('#qty').on('keyup', function(event) {
-        event.preventDefault();
-        var unit_cost = parseFloat($('#unit_cost').val())
-        var qty = parseFloat($(this).val())
-        $('#total_cost').val((unit_cost * qty).toFixed(2))
-    });
-
-    $('#newPurchaseFrm').on('submit', function(event) {
-        event.preventDefault();
-
-        $(this).submit(function(event) {
-            return false;
+        $('#date_issued, #due_date').on('changeDate', function() {
+            $(this).datepicker('hide');
         })
 
-        $.ajax({
-            url: '../serverscripts/admin/Suppliers/newPurchaseFrm.php',
-            type: 'POST',
-            data: $('#newPurchaseFrm').serialize(),
-            success: function(msg) {
-                if (msg === 'save_successful') {
-                    bootbox.alert("Purchase Recorded Successfully", function() {
-                        window.location = 'prepareSupplierInvoice.php';
-                    })
-                } else {
-                    bootbox.alert(msg)
-                }
-            }
-        }) //end ajax
-
-    });
-
-    $('#newPaymentFrm').on('submit', function(event) {
-        event.preventDefault();
-        $.ajax({
-            url: '../serverscripts/admin/Suppliers/newPaymentFrm.php',
-            type: 'POST',
-            data: $('#newPaymentFrm').serialize(),
-            success: function(msg) {
-                if (msg === 'save_successful') {
-                    bootbox.alert("Payment Recorded Successfully", function() {
-                        window.location.reload()
-                    })
-                } else {
-                    bootbox.alert(msg)
-                }
-            }
+        $('#new_payment_modal').on('shown.bs.modal', function() {
+            setTimeout(function() {
+                $('#amount_paid').focus();
+            }, 300)
         })
-    });
 
-});
+        $('#qty').on('keyup', function(event) {
+            event.preventDefault();
+            var unit_cost = parseFloat($('#unit_cost').val())
+            var qty = parseFloat($(this).val())
+            $('#total_cost').val((unit_cost * qty).toFixed(2))
+        });
+        
+    });
 </script>
 @endsection
