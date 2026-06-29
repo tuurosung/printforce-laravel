d84e972b66e1c420a272224a0f593c9829d0cf80
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-29 22:02:45 +0000

    Add Relationships to OtherJob Model for Customer and Service
    - Introduced relationships in the OtherJob model to link it with Customer and PrintService models, enhancing data retrieval capabilities.
    - Implemented the customer() and service() methods to establish BelongsTo relationships, improving the overall structure and maintainability of the model.
    - These changes support better integration and interaction within the print jobs domain, aligning with ongoing refactoring efforts.

--- app/Domain/PrintJobs/Models/OtherJob.php
+++ app/Domain/PrintJobs/Models/OtherJob.php
@@ -2,14 +2,29 @@
 
 namespace App\Domain\PrintJobs\Models;
 
+use App\Domain\PrintServices\Models\PrintService;
+use App\Models\Customers\Customer;
 use App\Observers\Jobs\OtherJobObserver;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Resend\Service\Service;
 
 #[ObservedBy([OtherJobObserver::class])]
 #[Fillable(["id","subscriber_id", "customer_id", "job_id", "service_id", "cost", "qty", "total", "date", "job_assigned_by", "job_assigned_at", "job_completed_at", "job_completed_at"])]
 class OtherJob extends Model
 {
     protected $table = "jobs_other";
+
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class, 'customer_id');
+    }
+
+    public function service(): BelongsTo
+    {
+        return $this->belongsTo(PrintService::class,'service_id');
+    }
 }
