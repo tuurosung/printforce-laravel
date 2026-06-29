4669fa1147873349e71c6d5607a6fe3e109a6697
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-29 08:47:16 +0000

    Refactor app layout for improved structure and theme integration
    - Updated the HTML structure of the app layout to enhance readability and maintainability.
    - Introduced a new main wrapper and improved the organization of sidebar and top navigation components.
    - Implemented a default theme class for better styling consistency across the application.
    - Added JavaScript functions for handling color themes, enhancing user customization options.
    - This refactor improves the overall layout and user experience of the application.

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,21 +1,70 @@
 @include('layout.partials.header')
 
-<body class="pace-done">
-    <div class="app" id="app">
-        @include('layout.topnav')
-        @include('layout.sidebar')
+<body class="DEFAULT_THEME bg-white dark:bg-dark">
 
-        <div class="app-content" id="content">
-            <div class="container-fluid">
 
-                @yield('content')
+    <main>
+        <!--start the project-->
+        <div id="main-wrapper" class="flex">
 
+            @include('layout.partials.application-sidebar')
+
+
+            <div class="page-wrapper w-full" role="main">
+
+                @include('layout.partials.application-top-nav')
+
+
+
+                <!-- Main Content -->
+                <main class="pt-6" style="background-color: #f4f7fb !important;">
+                    <div class="container full-container py-5">
+                        @include('layout.errors')
+
+                        @yield('content')
+
+
+                    </div>
+                </main>
+                <!-- Main Content End -->
             </div>
+
+
+
         </div>
+        <!--end of project-->
+    </main>
 
-    </div>
+
+
+    <script>
+        function handleColorTheme(e) {
+            document.documentElement.setAttribute("data-color-theme", e);
+        }
+    </script>
+
+
+    <!-- <script src="../assets/libs/prismjs/prism.js"></script> -->
+    <!-- For Datatables -->
+    <script src="{{ asset('vendor/vendor.min.js') }}"></script>
+    <!-- <script src="{{ asset('vendor/tailwindcss.js') }}"></script> -->
+    <!-- <script src="https://flowbite.com/docs/flowbite.min.js?v=4.0.1a"></script> -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> -->
+    <script src="{{ asset('vendor/loadash.js') }}"></script>
+    <script src="{{ asset('vendor/vanilla-calenda.js') }}"></script>
+    <!-- For Datatables Init-->
+    <script>
+        // new DataTable('.datatables', {
+        //     scrollX: true,
+        // });
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/@floating-ui/core@1.7.5"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@floating-ui/dom@1.7.6"></script>
+
+    @livewireScripts
 </body>
-
 @include('layout.partials.footer')
-
 @yield('js')
+
+</html>
