2c1e149d705a30acbbea66ba604f740eee9fe110
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-30 21:27:31 +0000

    Refactor TodaysJobsController to Utilize PrintJobService for Job Retrieval
    - Updated TodaysJobsController to use the recentJobs method from PrintJobService, enhancing job retrieval efficiency.
    - Injected CustomerService into the controller to fetch customer data, improving the overall functionality of the job view.
    - Adjusted statistics calculations to reflect recent jobs, ensuring accurate data representation in the view.
    - These changes support ongoing refactoring efforts within the print jobs domain, improving maintainability and code clarity.

--- app/Domain/PrintJobs/Http/Controllers/TodaysJobsController.php
+++ app/Domain/PrintJobs/Http/Controllers/TodaysJobsController.php
@@ -2,27 +2,38 @@
 
 namespace App\Domain\PrintJobs\Http\Controllers;
 
+use App\Domain\Customers\Services\CustomerService;
 use App\Domain\PrintJobs\Models\PrintforceJob;
+use App\Domain\PrintJobs\Services\PrintJobService;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class TodaysJobsController extends Controller
 {
+
+    public function __construct(
+        public PrintJobService $printJobService,
+        private CustomerService $customerService
+    ){}
+
+
     /**
      * Handle the incoming request.
      */
     public function __invoke(Request $request)
     {
-        $allJobs = PrintforceJob::limit(100)->get(); //TODO Add a service to handle this
+
+        $recentJobs = $this->printJobService->recentJobs();
 
         $statistics = [
-            'total' => $allJobs->sum('total'),
-            'todays_jobs_total' => $allJobs->where('created_at', today())->count(),
+            'total' => $recentJobs->sum('total'),
+            'todays_jobs_total' => $recentJobs->where('created_at', today())->count(),
         ];
 
         return view('app.job.todays-jobs', [
-            'allJobs' => $allJobs,
-            'statistics' => $statistics
+            'allJobs' => $recentJobs,
+            'statistics' => $statistics,
+            'customers' => $this->customerService->getCustomersArray(),
         ]);
     }
 }
