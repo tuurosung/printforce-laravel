023423c21fb64cf1b2c1d069b36973bcd0bf5faf
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-07-01 02:50:55 +0000

    Refactor TodaysJobsController to Simplify Job View Logic
    - Removed dependency on PrintJobService and CustomerService, streamlining the controller's functionality.
    - Simplified the __invoke method to directly return the view without additional data processing.
    - These changes enhance code clarity and reduce complexity in the TodaysJobsController, supporting ongoing improvements in the print jobs domain.

--- app/Domain/PrintJobs/Http/Controllers/TodaysJobsController.php
+++ app/Domain/PrintJobs/Http/Controllers/TodaysJobsController.php
@@ -2,38 +2,17 @@
 
 namespace App\Domain\PrintJobs\Http\Controllers;
 
-use App\Domain\Customers\Services\CustomerService;
-use App\Domain\PrintJobs\Models\PrintforceJob;
-use App\Domain\PrintJobs\Services\PrintJobService;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class TodaysJobsController extends Controller
 {
 
-    public function __construct(
-        public PrintJobService $printJobService,
-        private CustomerService $customerService
-    ){}
-
-
     /**
      * Handle the incoming request.
      */
     public function __invoke(Request $request)
     {
-
-        $recentJobs = $this->printJobService->recentJobs();
-
-        $statistics = [
-            'total' => $recentJobs->sum('total'),
-            'todays_jobs_total' => $recentJobs->where('created_at', today())->count(),
-        ];
-
-        return view('app.job.todays-jobs', [
-            'allJobs' => $recentJobs,
-            'statistics' => $statistics,
-            'customers' => $this->customerService->getCustomersArray(),
-        ]);
+        return view('app.job.todays-jobs');
     }
 }
