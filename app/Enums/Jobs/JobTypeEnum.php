7eb64641182bc5af39872e2ffd8bd7726f2e70e5
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-07-11 02:31:48 +0000

    Add JobTypeEnum for Job Management
    - Introduced JobTypeEnum to define various job types including Large Format, Embroidery, Press, Design, Photography, and Others.
    - Implemented methods for job table mapping, job codes, labels, and additional properties such as dimensionality and material requirements.
    - This addition enhances job management capabilities within the application, providing a structured approach to handling different job types.

---
+++ app/Enums/Jobs/JobTypeEnum.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Enums\Jobs;
+
+enum JobTypeEnum: string
+{
+    case LargeFormat = "large_format";
+    case Embroidery = "embroidery";
+    case Press = "press";
+    case Design = "design";
+    case Photography = "photography";
+    case Others = "others";
+
+
+    public function jobTable()
+    {
+        return match ($this) {
+            self::LargeFormat => "jobs_largeformat",
+            self::Embroidery => "jobs_embroidery",
+            self::Press => "jobs_press",
+            self::Design => "jobs_design",
+            self::Photography => "jobs_photography",
+            self::Others => "jobs_other",
+        };
+    }
+
+
+    public function jobCode(): string
+    {
+        return match ($this) {
+            self::LargeFormat => "001",
+            self::Design => "002",
+            self::Embroidery => "003",
+            self::Press => "004",
+            self::Photography => "005",
+            self::Others => "006"
+        };
+    }
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::LargeFormat => "Large Format Jobs",
+            self::Design => "Design Jobs",
+            self::Embroidery => "Embroidery Jobs",
+            self::Press => "Press Jobs",
+            self::Photography => "Photography Jobs",
+            self::Others => "Other Jobs",
+        };
+    }
+
+
+    public function dimensional(): bool
+    {
+        return match ($this) {
+            self::LargeFormat, self::Press => true,
+            default => false,
+        };
+    }
+
+    public function hasMaterials(): bool
+    {
+        return $this === self::Embroidery;
+    }
+
+    public function isNotCost(): bool
+    {
+        return match ($this) {
+            self::Embroidery => true,
+            self::Design => true,
+            self::Photography => true,
+            default => false,
+        };
+    }
+
+    public function isCopies(): bool
+    {
+        return match ($this) {
+            self::LargeFormat => true,
+            self::Design => true,
+            self::Photography => true,
+            self::Press => true,
+            default => false,
+        };
+    }
+}
