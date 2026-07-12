5affca9404be5a888b8724ec22cce4ce4883f1a1
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-07-12 03:37:56 +0000

    Add CustomerPaymentObserver for Payment Event Handling
    - Introduced CustomerPaymentObserver to manage events related to CustomerPayment model, including creating, created, updated, deleted, restored, and force deleted events.
    - Implemented logic to generate a unique payment ID during the creation of a CustomerPayment, enhancing data integrity.
    - This addition lays the groundwork for improved handling of customer payment events within the application.

---
+++ app/Observers/CustomerPaymentObserver.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Observers;
+
+use App\Domain\Payments\Models\CustomerPayment;
+
+class CustomerPaymentObserver
+{
+
+    public function creating(CustomerPayment $customerPayment)
+    {
+            $customerPayment->payment_id = generateDashedRandomNumber();
+            // $customerPayment->subscriber_id = Auth::user()->subscriber_id;
+    }
+
+
+    /**
+     * Handle the CustomerPayment "created" event.
+     */
+    public function created(CustomerPayment $customerPayment): void
+    {
+        //
+    }
+
+    /**
+     * Handle the CustomerPayment "updated" event.
+     */
+    public function updated(CustomerPayment $customerPayment): void
+    {
+        //
+    }
+
+    /**
+     * Handle the CustomerPayment "deleted" event.
+     */
+    public function deleted(CustomerPayment $customerPayment): void
+    {
+        //
+    }
+
+    /**
+     * Handle the CustomerPayment "restored" event.
+     */
+    public function restored(CustomerPayment $customerPayment): void
+    {
+        //
+    }
+
+    /**
+     * Handle the CustomerPayment "force deleted" event.
+     */
+    public function forceDeleted(CustomerPayment $customerPayment): void
+    {
+        //
+    }
+}
